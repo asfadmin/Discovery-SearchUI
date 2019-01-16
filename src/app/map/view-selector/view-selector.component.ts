5966c125a6d30c581953608602cc5a230cf07694
William Horn <wbhorn@alaska.edu>
2019-01-16 12:12:03 -0900

    Fix position class name and move view selector html into template file

--- src/app/map/view-selector/view-selector.component.ts
+++ src/app/map/view-selector/view-selector.component.ts
@@ -4,20 +4,7 @@ import { MapViewType } from '../../models';
 
 @Component({
   selector: 'app-view-selector',
-  template: `
-    <mat-button-toggle-group class="position-1" name="viewSelect" aria-label="Font Style">
-      <mat-button-toggle (click)="onArcticSelected()" >
-        <div class="view-btn arctic"></div>
-      </mat-button-toggle>
-      <mat-button-toggle (click)="onEquitorialSelected()" >
-        <div class="view-btn equatorial"></div>
-      </mat-button-toggle>
-
-      <mat-button-toggle (click)="onAntarcticSelected()" >
-        <div class="view-btn antarctic"></div>
-      </mat-button-toggle>
-    </mat-button-toggle-group>
-  `,
+  templateUrl: './view-selector.component.html',
   styleUrls: ['./view-selector.component.css']
 })
 export class ViewSelectorComponent {
