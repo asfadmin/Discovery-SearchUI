4b4fb2a524ff4f39b2da65137fe7ace714c06cbb
William Horn <wbhorn@alaska.edu>
2019-01-17 13:34:00 -0900

    Prompt the user when they copy a granule name

--- src/app/filters-menu/products/sentinel1-product/product-name/product-name.component.ts
+++ src/app/filters-menu/products/sentinel1-product/product-name/product-name.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { of } from 'rxjs';
+import { tap, delay } from 'rxjs/operators';
+
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
 import { ClipboardService } from 'ngx-clipboard';
 
@@ -13,6 +16,11 @@ export class ProductNameComponent {
 
   constructor(private clipboardService: ClipboardService) {}
 
+  public copyPrompt =  'Copy to clipboard';
+  public copyNotification = 'Copied!';
+
+  public copyTooltipMessage = this.copyPrompt;
+
   public copyIcon = faCopy;
   public isNameHovered = false;
 
@@ -22,6 +30,13 @@ export class ProductNameComponent {
 
   onCopyIconClicked(e: Event): void {
     this.clipboardService.copyFromContent(this.name);
+
+    of(this.copyPrompt).pipe(
+      tap(() => this.copyTooltipMessage = this.copyNotification),
+      delay(2200)
+    ).subscribe(
+      msg => this.copyTooltipMessage = msg
+    );
 
     e.stopPropagation();
   }
