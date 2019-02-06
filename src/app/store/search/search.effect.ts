a03bab16f0a205db63d92f19b1cf35ceea067e2c
William Horn <wbhorn@alaska.edu>
2019-02-05 15:33:40 -0900

    Refactor search effect into multiple functions

--- src/app/store/search/search.effect.ts
+++ src/app/store/search/search.effect.ts
@@ -25,90 +25,50 @@ export class SearchEffects {
     private store$: Store<AppState>,
     private asfApiService: services.AsfApiService,
     private mapService: services.MapService,
-  ) {
-  }
+  ) {}
 
   @Effect()
   private makeSearches: Observable<Action> = this.actions$.pipe(
     ofType(SearchActionType.MAKE_SEARCH),
 
-      withLatestFrom(this.searchState$()),
-      map(([_, searchState]) => searchState),
-      map(
-        ([listParam, filterParams]) => {
-          const params = (listParam.granule_list.length > 0) ?
-            listParam :
-            filterParams;
-
-          return Object.entries(params)
-            .filter(([param, val]) => !!val)
-            .reduce(
-              (queryParams, [param, val]) => queryParams.append(param, <string>val),
-              new HttpParams()
-            );
-        }),
-      switchMap(
-        params => this.asfApiService.query(params).pipe(
-          tap(console.log),
-          map(makeGranules),
-          map(granules => new granulesStore.SetGranules(granules))
-        )
+    withLatestFrom(this.searchParams$()),
+    map(([_, params]) => params),
+    switchMap(
+      params => this.asfApiService.query(params).pipe(
+        map(makeGranules),
+        map(granules => new granulesStore.SetGranules(granules))
       )
+    )
   );
 
-  private searchState$() {
+  private searchParams$() {
     return combineLatest(
-
-      this.store$.select(granulesStore.getGranuleSearchList).pipe(
-        map(granuleList => ({ granule_list: granuleList.join(',') }))
+      this.granuleListParam$(),
+      this.filterSearchParams$()
+    ).pipe(
+      map(
+        ([listParam, filterParams]) =>
+        listParam.granule_list.length > 0 ?
+          listParam :
+          filterParams
       ),
-
-      combineLatest(
-        combineLatest(
-          this.mapService.searchPolygon$.pipe(startWith(null)),
-          this.shouldOmitSearchPolygon$()
-        ).pipe(
-          map(([polygon, shouldOmitGeoRegion]) => shouldOmitGeoRegion ? null : polygon),
-          map(polygon => ({ intersectsWith: polygon }))
-        ),
-        this.store$.select(filterStore.getSelectedPlatforms).pipe(
-          map(platforms => platforms
-            .map(platform => platform.name)
-            .join(',')
-            .replace('ALOS PALSAR', 'ALOS')
-          ),
-          map(platforms => ({ platform: platforms }))
-        ),
-        this.store$.select(filterStore.getDateRange).pipe(
-          map(range => {
-            return [range.start, range.end]
-              .filter(date => !!date)
-              .map(date => date.toISOString());
-          }),
-          map(([start, end]) => ({ start, end }))
-        ),
-        this.store$.select(filterStore.getPathRange).pipe(
-          map(range => Object.values(range)
-            .filter(v => !!v)
-          ),
-          map(range => Array.from(new Set(range))),
-          map(range => range.length === 2 ?
-            range.join('-') :
-            range.pop() || null
-          ),
-          map(pathRange => ({ relativeOrbit: pathRange }))
-        ),
-        this.store$.select(filterStore.getFrameRange).pipe(
-          map(range => Object.values(range)
-            .filter(v => !!v)
-          ),
-          map(range => Array.from(new Set(range))),
-          map(range => range.length === 2 ?
-            range.join('-') :
-            range.pop() || null
-          ),
-          map(frameRange => ({ frame: frameRange }))
+      map(params => Object.entries(params)
+        .filter(([param, val]) => !!val)
+        .reduce(
+          (queryParams, [param, val]) => queryParams.append(param, <string>val),
+          new HttpParams()
         )
+      )
+    );
+  }
+
+  private filterSearchParams$() {
+    return combineLatest(
+        this.searchPolygon$(),
+        this.selectedPlatforms$(),
+        this.dateRange$(),
+        this.pathRange$(),
+        this.frameRange$(),
       ).pipe(
         map(params => params
           .filter(param => !!Object.values(param)[0])
@@ -116,13 +76,77 @@ export class SearchEffects {
             (total, param) =>  ({...total, ...param}),
             {})
         )
-      )
     );
   }
 
-  private shouldOmitSearchPolygon$() {
-    return this.store$.select(filterStore.getShouldOmitSearchPolygon);
+  private granuleListParam$() {
+    return this.store$.select(granulesStore.getGranuleSearchList).pipe(
+      map(granuleList => ({ granule_list: granuleList.join(',') }))
+    );
   }
+
+  private searchPolygon$() {
+    return combineLatest(
+      this.mapService.searchPolygon$.pipe(startWith(null)),
+      this.store$.select(filterStore.getShouldOmitSearchPolygon)
+    ).pipe(
+      map(([polygon, shouldOmitGeoRegion]) => shouldOmitGeoRegion ? null : polygon),
+      map(polygon => ({ intersectsWith: polygon }))
+    );
+  }
+
+  private selectedPlatforms$() {
+
+    return this.store$.select(filterStore.getSelectedPlatforms).pipe(
+      map(platforms => platforms
+        .map(platform => platform.name)
+        .join(',')
+        .replace('ALOS PALSAR', 'ALOS')
+      ),
+      map(platforms => ({ platform: platforms }))
+    );
+  }
+
+  private dateRange$() {
+
+    return this.store$.select(filterStore.getDateRange).pipe(
+      map(range => {
+        return [range.start, range.end]
+          .filter(date => !!date)
+          .map(date => date.toISOString());
+      }),
+      map(([start, end]) => ({ start, end }))
+    );
+  }
+
+  private pathRange$() {
+    return this.store$.select(filterStore.getPathRange).pipe(
+      map(range => Object.values(range)
+        .filter(v => !!v)
+      ),
+      map(range => Array.from(new Set(range))),
+      map(range => range.length === 2 ?
+        range.join('-') :
+        range.pop() || null
+      ),
+      map(pathRange => ({ relativeOrbit: pathRange }))
+    );
+  }
+
+  private frameRange$() {
+    return this.store$.select(filterStore.getFrameRange).pipe(
+      map(range => Object.values(range)
+        .filter(v => !!v)
+      ),
+      map(range => Array.from(new Set(range))),
+      map(range => range.length === 2 ?
+        range.join('-') :
+        range.pop() || null
+      ),
+      map(frameRange => ({ frame: frameRange }))
+    );
+  }
+
 }
 
 const makeGranules =
