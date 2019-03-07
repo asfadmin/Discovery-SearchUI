8603418650f5dd978544204be6da7d2f32eb13c8
William Horn <wbhorn@alaska.edu>
2019-03-06 16:31:26 -0900

    Start using marble observables for testin

--- src/app/services/url-state.service.spec.ts
+++ src/app/services/url-state.service.spec.ts
@@ -1,5 +1,7 @@
-import { async, TestBed, inject } from '@angular/core/testing';
+import { async, fakeAsync, tick, TestBed, inject } from '@angular/core/testing';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import { cold, getTestScheduler } from 'jasmine-marbles';
 
 import { Subject, BehaviorSubject } from 'rxjs';
 import { Store } from '@ngrx/store';
@@ -15,11 +17,49 @@ import { WktService } from './wkt.service';
 import { RangeService } from './range.service';
 
 
+describe('UrlStateService', () => {
+  let service: UrlStateService;
+  let store: TestStore<AppState>;
+  let routerNavigateSpy;
+  let dispatchSpy;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        UrlStateService,
+        WktService,
+        RangeService,
+        { provide: MapService, useClass: TestMapService },
+        { provide: Store, useClass: TestStore },
+        { provide: Router, useClass: TestRouter },
+        { provide: ActivatedRoute, useClass: TestActivatedRoute },
+      ]
+    });
+    service = TestBed.get(UrlStateService);
+  });
+
+  beforeEach(inject([Store], (testStore: TestStore<AppState>) => {
+    store = testStore;
+    dispatchSpy = spyOn(store, 'dispatch');
+    store.setState(defaultAppState);
+  }));
+
+  beforeEach(inject([Router], (testRouter: TestRouter) => {
+    routerNavigateSpy = spyOn(testRouter, 'navigate');
+  }));
+
+  it('should run #load()', () => {
+    getTestScheduler().flush();
+    service.load();
+    expect(routerNavigateSpy).toHaveBeenCalled();
+  });
+});
+
 class TestMapService {
-  public zoom$ = new BehaviorSubject<number>(5);
-  public center$ = new BehaviorSubject<models.LonLat>({lon: 10, lat: 10});
+  public zoom$ = cold('-vv', {v: 5});
+  public center$ = cold('-vv', {v: {lon: 10, lat: 10}});
   public epsg$ = new BehaviorSubject<string>('EPSG');
-  public searchPolygon$ = new BehaviorSubject<string>('wkt here...');
+  public searchPolygon$ = new BehaviorSubject<string | null>(null);
   public mousePosition$ = new BehaviorSubject<models.LonLat>({
     lon: 0, lat: 0
   });
@@ -38,37 +78,5 @@ class TestRouter {
 }
 
 class TestActivatedRoute {
-  public queryParams = new BehaviorSubject<string>('');
+  public queryParams = cold('-q', {zoom: '1'});
 }
-
-fdescribe('UrlStateService', () => {
-  let service: UrlStateService;
-  let store: TestStore<AppState>;
-  let dispatchSpy;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        UrlStateService,
-        WktService,
-        RangeService,
-        { provide: MapService, useClass: TestMapService },
-        { provide: Store, useClass: TestStore },
-        { provide: Router, useClass: TestRouter },
-        { provide: ActivatedRoute, useClass: TestActivatedRoute },
-      ]
-    });
-
-    service = TestBed.get(UrlStateService);
-  });
-
-  beforeEach(inject([Store], (testStore: TestStore<AppState>) => {
-    store = testStore;
-    dispatchSpy = spyOn(store, 'dispatch');
-    store.setState(defaultAppState);
-  }));
-
-  it('should run #load()', async () => {
-    // load();
-  });
-});
