0829006def3f3a77099cd3d1a6b96e843a5dd57d
William Horn <wbhorn@alaska.edu>
2019-03-18 12:18:21 -0800

    Observable to close window when it is redirected back to state url

--- src/app/components/map/nav-bar/nav-bar.component.ts
+++ src/app/components/map/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
-import { map } from 'rxjs/operators';
+import { interval, Subject } from 'rxjs';
+import { map, takeUntil, tap, delay, take } from 'rxjs/operators';
 
 import { Store } from '@ngrx/store';
 
@@ -24,15 +25,32 @@ export class NavBarComponent {
 
   public onEarthdataLogin(): void {
     const vertexUrl = window.location.origin;
+    const appRedirect = `${vertexUrl}?uiView=${ViewType.LOGIN}`;
+
     const url = 'https://urs.earthdata.nasa.gov/oauth/authorize' +
       '?client_id=BO_n7nTIlMljdvU6kRRB3g&response_type=code' +
       '&redirect_uri=https://vertex.daac.asf.alaska.edu/services/urs4_token_request' +
-      `&state=${vertexUrl}?uiView=${ViewType.LOGIN}`;
+      `&state=${appRedirect}`;
 
     const loginWindow = window.open(
       url,
       'Vertex: URS Earth Data Authorization',
       'scrollbars=yes, width=600, height= 600'
+    );
+
+    const loginDone = new Subject();
+
+    const closeWindow = interval(500).pipe(
+      take(50),
+      takeUntil(loginDone),
+    ).subscribe(_ => {
+        try {
+          if (loginWindow.location.href === appRedirect) {
+            loginDone.next();
+          }
+        } catch (e) {
+        }
+      }
     );
   }
 }
