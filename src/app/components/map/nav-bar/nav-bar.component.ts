5ad9643f27ab6ff0d55c5abdd4d45fd9b306f74c
William Horn <wbhorn@alaska.edu>
2019-03-18 11:57:13 -0800

    Start login process when login button is clicked in nav bar

--- src/app/components/map/nav-bar/nav-bar.component.ts
+++ src/app/components/map/nav-bar/nav-bar.component.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 
 import { AppState } from '@store';
 import * as queueStore from '@store/queue';
-import { Sentinel1Product } from '@models';
+import { Sentinel1Product, ViewType } from '@models';
 
 @Component({
   selector: 'app-nav-bar',
@@ -20,5 +20,19 @@ export class NavBarComponent {
 
   public onOpenDownloadQueue(): void {
     this.openQueue.emit();
+  }
+
+  public onEarthdataLogin(): void {
+    const vertexUrl = window.location.origin;
+    const url = 'https://urs.earthdata.nasa.gov/oauth/authorize' +
+      '?client_id=BO_n7nTIlMljdvU6kRRB3g&response_type=code' +
+      '&redirect_uri=https://vertex.daac.asf.alaska.edu/services/urs4_token_request' +
+      `&state=${vertexUrl}?uiView=${ViewType.LOGIN}`;
+
+    const loginWindow = window.open(
+      url,
+      'Vertex: URS Earth Data Authorization',
+      'scrollbars=yes, width=600, height= 600'
+    );
   }
 }
