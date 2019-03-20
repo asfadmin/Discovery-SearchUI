17fb2e358e747bc4648f1fae0bca27fbdf0a3356
Mariya Shapran <mshapran@alaska.edu>
2019-03-19 19:18:37 -0700

    DS-2048: latest karma config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,34 +1,32 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision
-const Downloader = require('puppeteer/utils/ChromiumDownloader')
-const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision)
-
-process.env.CHROME_BIN = puppeteer.executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-jasmine-html-reporter'),
       require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
+    files: ['tests.js'],
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
-    singleRun: true
+    customLaunchers: {
+        HeadlessChrome: {
+            base: 'ChromeHeadless',
+            flags: [
+	      '--no-sandbox',
+	      '--headless',
+              '--disable-gpu',
+              '--disable-translate',
+              '--disable-extensions'
+	    ]
+  	}
+    },	
+    singleRun: true,
+    browserNoActivityTimeout: 120000,
+    urlRoot: ''
   });
 };
