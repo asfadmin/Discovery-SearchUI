ee8940ad28ac6f767c5169dce08e594c87c0d3bc
Mariya Shapran <mshapran@alaska.edu>
2019-03-19 18:54:35 -0700

    DS-2048: trying out Chromium Headless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision
+const Downloader = require('puppeteer/utils/ChromiumDownloader')
+const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision)
+
+process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -7,23 +12,15 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
-      require('karma-webpack'),
       require('@angular-devkit/build-angular')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    preprocessors: {
-      './src/test.ts': ['webpack']
-    },
-    mime:  {
-      'text/x-typescript': ['@angular-devkit/build-angular']
-    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
@@ -31,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     singleRun: true
   });
 };
