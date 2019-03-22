0256c93bf30da3b52d6235ac5e662b87b6d3c339
William Horn <wbhorn@alaska.edu>
2019-03-22 12:14:26 -0800

    Fix broken test modules

--- src/app/services/datapool-auth.service.spec.ts
+++ src/app/services/datapool-auth.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DatapoolAuthService } from './datapool-auth.service';
 
 describe('DatapoolAuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+      imports: [ HttpClientModule ]
+  }));
 
   it('should be created', () => {
     const service: DatapoolAuthService = TestBed.get(DatapoolAuthService);
