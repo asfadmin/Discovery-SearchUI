600418cc048ddd4ab85256c59a6482f42c929dff
William Horn <wbhorn@alaska.edu>
2019-04-12 12:13:51 -0800

    Starting to move results into bottom menu
    Currently have same list component, still need to hook up product
    queueing and make a better granule detail card

--- src/app/components/bottom-menu/bottom-menu.module.ts
+++ src/app/components/bottom-menu/bottom-menu.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatSharedModule } from '@shared';
+import { TruncateModule } from '@yellowspot/ng-truncate';
 
+import { MatSharedModule } from '@shared';
+import { GranulesListModule } from '@components/sidebar/results/granules-list';
 import { BottomMenuComponent } from './bottom-menu.component';
 
 @NgModule({
   declarations: [BottomMenuComponent],
   imports: [
     CommonModule,
-    MatSharedModule
+    TruncateModule,
+    MatSharedModule,
+    GranulesListModule,
   ],
   exports: [BottomMenuComponent],
 })
