d277c18053d75c1c6c3071b5d4c96c0878da4ac5
William Horn <wbhorn@alaska.edu>
2019-04-29 09:33:26 -0800

    First draft of granule detail pane in bottom menu

--- src/app/components/bottom-menu/bottom-menu.module.ts
+++ src/app/components/bottom-menu/bottom-menu.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TruncateModule } from '@yellowspot/ng-truncate';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { PipesModule } from '@pipes';
 import { MatSharedModule } from '@shared';
 import { GranulesListModule } from '@components/sidebar/results/granules-list';
 import { BottomMenuComponent } from './bottom-menu.component';
@@ -11,8 +13,10 @@ import { BottomMenuComponent } from './bottom-menu.component';
   declarations: [BottomMenuComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     TruncateModule,
     MatSharedModule,
+    PipesModule,
     GranulesListModule,
   ],
   exports: [BottomMenuComponent],
