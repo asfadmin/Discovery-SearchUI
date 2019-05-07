b98c2823d35189356502cb644a096cb5f4fcbc5b
William Horn <wbhorn@alaska.edu>
2019-05-07 11:20:45 -0800

    Remove all unnecessary store code from breadcrumb-list/dataset search

--- src/app/components/map/nav-bar/breadcrumb-list/breadcrumb-list.component.ts
+++ src/app/components/map/nav-bar/breadcrumb-list/breadcrumb-list.component.ts
@@ -33,11 +33,7 @@ enum BreadcrumbFilterType {
 export class BreadcrumbListComponent {
   @Output()  doSearch = new EventEmitter<void>();
 
-  constructor(
-    private store$: Store<AppState>,
-    private dateExtremaService: services.DateExtremaService,
-    private mapService: services.MapService,
-  ) { }
+  constructor(private store$: Store<AppState>) { }
 
   public filterTypes = BreadcrumbFilterType;
   public selectedFilter = BreadcrumbFilterType.NONE;
@@ -48,50 +44,6 @@ export class BreadcrumbListComponent {
   public loading$ = this.store$.select(searchStore.getIsLoading);
   public searchType$ = this.store$.select(uiStore.getSearchType);
   public searchTypes = models.SearchType;
-
-  // Platform Selector
-  public platforms$ = this.store$.select(filtersStore.getPlatformsList);
-  public selectedPlatformName$ = this.store$.select(filtersStore.getSelectedPlatformNames).pipe(
-    map(platform => platform.size === 1 ?
-      platform.values().next().value : null
-    )
-  );
-
-  // Date Selector
-  public startDate$ = this.store$.select(filtersStore.getStartDate);
-  public endDate$ = this.store$.select(filtersStore.getEndDate);
-  public seasonStart$ = this.store$.select(filtersStore.getSeasonStart);
-  public seasonEnd$ = this.store$.select(filtersStore.getSeasonEnd);
-  public selectedPlatforms$ = this.store$.select(filtersStore.getSelectedPlatforms);
-
-  public dateRangeExtrema$ = this.dateExtremaService.getExtrema$(
-    this.platforms$,
-    this.selectedPlatforms$,
-    this.startDate$,
-    this.endDate$,
-  );
-
-  // AOI Selector
-  public drawMode$ = this.store$.select(mapStore.getMapDrawMode);
-  public interactionMode$ = this.store$.select(mapStore.getMapInteractionMode);
-
-  public polygon$ = this.mapService.searchPolygon$;
-
-  // Path/Frame Selector
-  public pathRange$ = this.store$.select(filtersStore.getPathRange);
-  public frameRange$ = this.store$.select(filtersStore.getFrameRange);
-  public shouldOmitSearchPolygon$ = this.store$.select(filtersStore.getShouldOmitSearchPolygon);
-
-  // Additional Filters Selector
-  public flightDirections$ = this.store$.select(filtersStore.getFlightDirections);
-  public beamModes$ = this.store$.select(filtersStore.getBeamModes);
-  public polarizations$ = this.store$.select(filtersStore.getPolarizations);
-  public platformProductTypes$ = this.store$.select(filtersStore.getProductTypes);
-
-  public onNewSelectedFilter(filterType: BreadcrumbFilterType): void {
-    this.selectedFilter = this.selectedFilter === filterType ?
-      BreadcrumbFilterType.NONE : filterType;
-  }
 
   public onDoSearch(): void {
     this.doSearch.emit();
