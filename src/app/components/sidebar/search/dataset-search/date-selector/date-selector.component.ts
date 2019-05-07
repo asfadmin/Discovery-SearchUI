a1df992ad44a37b101edf1d65ed247fc8ad8e88a
William Horn <wbhorn@alaska.edu>
2019-05-07 10:34:20 -0800

    Move store logic into date selector component

--- src/app/components/sidebar/search/dataset-search/date-selector/date-selector.component.ts
+++ src/app/components/sidebar/search/dataset-search/date-selector/date-selector.component.ts
@@ -2,45 +2,72 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { Observable } from 'rxjs';
 
+import { Store } from '@ngrx/store';
+
+import { AppState } from '@store';
+import * as filtersStore from '@store/filters';
+
 import { Platform, DateRangeExtrema } from '@models';
+import { DateExtremaService } from '@services';
 
 @Component({
   selector: 'app-date-selector',
   templateUrl: './date-selector.component.html',
   styleUrls: ['./date-selector.component.scss']
 })
-export class DateSelectorComponent {
-  @Input() startDate: Date;
-  @Input() endDate: Date;
-  @Input() seasonStart: number | null;
-  @Input() seasonEnd: number | null;
-  @Input() extrema: DateRangeExtrema;
-
-  @Output() newStart = new EventEmitter<Date>();
-  @Output() newEnd = new EventEmitter<Date>();
-  @Output() newSeasonStart = new EventEmitter<number>();
-  @Output() newSeasonEnd = new EventEmitter<number>();
+export class DateSelectorComponent implements OnInit {
+  public extrema: DateRangeExtrema;
+  public startDate: Date;
+  public endDate: Date;
 
   public isSeasonalSearch = false;
+
+  public platforms$ = this.store$.select(filtersStore.getPlatformsList);
+  public startDate$ = this.store$.select(filtersStore.getStartDate);
+  public endDate$ = this.store$.select(filtersStore.getEndDate);
+  public seasonStart$ = this.store$.select(filtersStore.getSeasonStart);
+  public seasonEnd$ = this.store$.select(filtersStore.getSeasonEnd);
+  public selectedPlatforms$ = this.store$.select(filtersStore.getSelectedPlatforms);
+
+  public dateRangeExtrema$ = this.dateExtremaService.getExtrema$(
+    this.platforms$,
+    this.selectedPlatforms$,
+    this.startDate$,
+    this.endDate$,
+  );
+
+  constructor(
+    private store$: Store<AppState>,
+    private dateExtremaService: DateExtremaService,
+  ) { }
+
+  ngOnInit() {
+    this.dateRangeExtrema$.subscribe(
+      extrema => this.extrema = extrema
+    );
+
+    this.startDate$.subscribe(start => this.startDate = start);
+    this.endDate$.subscribe(end => this.endDate = end);
+  }
 
   public onToggleSeasonalOptions(): void {
     this.isSeasonalSearch = !this.isSeasonalSearch;
   }
 
   public onStartDateChange(e: MatDatepickerInputEvent<Date>) {
-    this.newStart.emit(e.value);
+    this.store$.dispatch(new filtersStore.SetStartDate(e.value));
   }
 
   public onEndDateChange(e: MatDatepickerInputEvent<Date>) {
-    this.newEnd.emit(e.value);
+    this.store$.dispatch(new filtersStore.SetEndDate(e.value));
   }
 
   public onSeasonStartChange(dayOfYear: number): void {
-    this.newSeasonStart.emit(dayOfYear);
+    this.store$.dispatch(new filtersStore.SetSeasonStart(dayOfYear));
   }
 
   public onSeasonEndChange(dayOfYear: number): void {
-    this.newSeasonEnd.emit(dayOfYear);
+    this.store$.dispatch(new filtersStore.SetSeasonEnd(dayOfYear));
   }
 
   public dayOfYearFormat(dayOfYear: number | null): string {
