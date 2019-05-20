f15f8712c15675a31b94d8b791324eb5a4014650
Andrew Anderson <raanderson9@alaska.edu>
2019-05-20 15:22:32 -0700

    WIP on info bar, branding, and made cards and button square cornered.

--- src/app/components/sidebar/info-bar/info-bar.component.ts
+++ src/app/components/sidebar/info-bar/info-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-info-bar',
   templateUrl: './info-bar.component.html',
-  styleUrls: ['./info-bar.component.css']
+  styleUrls: ['./info-bar.component.scss']
 })
 export class InfoBarComponent implements OnInit {
 
--- src/app/components/sidebar/info-bar/info-bar.module.ts
+++ src/app/components/sidebar/info-bar/info-bar.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoBarComponent } from './info-bar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [InfoBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
   exports: [InfoBarComponent]
 })
