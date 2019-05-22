f65b44f85b4f277153290484092a9d166935cbf1
Andrew Anderson <raanderson9@alaska.edu>
2019-05-22 13:23:57 -0700

    WIP - got the side bar hiding happening in the logo.

--- src/app/components/sidebar/logo/logo.component.ts
+++ src/app/components/sidebar/logo/logo.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as uiStore from '@store/ui';
+import { Store } from '@ngrx/store';
+import { AppState } from '@store/app.reducer';
+
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit() {
+  public isSidebarOpen$ = this.store$.select(uiStore.getIsSidebarOpen);
 
+  constructor(
+    private store$: Store<AppState>,
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  public onToggleHide(): void {
+    this.store$.dispatch(new uiStore.ToggleSidebar());
   }
 
 }
