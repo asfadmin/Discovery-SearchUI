c2bcedae277d16cba22d94dde4369da9e5b685a6
Mariya Shapran <mshapran@alaska.edu>
2019-05-30 13:03:58 -0700

    removing examples

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,5 @@
+describe('The SearchUI Home Page', function() {
+  it('successfully loads', function() {
+    cy.visit('https://vertex-beta-dev.asf.alaska.edu')
+  })
+})
--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,4 +1,4 @@
-describe('Check SearchUI page', function () {
+describe('Check element on SearchUI dev page', function () {
   it('Visits the the dev SearchUI page', function () {
     cy.visit('https://vertex-beta-dev.asf.alaska.edu/')
   })
