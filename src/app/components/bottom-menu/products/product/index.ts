3fe2bdba9e3b231c021787d18ac07c87368e9d24
William Horn <wbhorn@alaska.edu>
2019-05-30 23:03:41 -0800

    Show/hide copy-to-clipboard when product is hovered
    Also add a tooltip to zoom to granule button

---
+++ src/app/components/bottom-menu/products/product/index.ts
@@ -0,0 +1,2 @@
+export { ProductComponent } from './product.component';
+export { ProductModule } from './product.module';
--- src/app/components/bottom-menu/products/products.module.ts
+++ src/app/components/bottom-menu/products/products.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TruncateModule } from '@yellowspot/ng-truncate';
 import { MatSharedModule } from '@shared';
-import { CopyToClipboardModule } from '@components/shared/copy-to-clipboard';
 import { ProductsComponent } from './products.component';
-import { PipesModule } from '@pipes';
+import { ProductModule } from './product';
 
 @NgModule({
   declarations: [ProductsComponent],
   imports: [
     CommonModule,
     MatSharedModule,
-    TruncateModule,
-    PipesModule,
-    CopyToClipboardModule,
+    ProductModule,
   ],
   exports: [
     ProductsComponent
