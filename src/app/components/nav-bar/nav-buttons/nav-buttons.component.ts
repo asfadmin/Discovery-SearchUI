fd9fe9038399522a09a604f6c68125319db9433e
Andrew Anderson <raanderson9@alaska.edu>
2019-06-05 06:36:16 -0700

    WIP breadcrumb works on medium and larger displays. Still doesn't work well with mobile, and still needs polish.

--- src/app/components/nav-bar/nav-buttons/nav-buttons.component.ts
+++ src/app/components/nav-bar/nav-buttons/nav-buttons.component.ts
@@ -8,7 +8,7 @@ import { CMRProduct } from '@models';
 @Component({
   selector: 'app-nav-buttons',
   templateUrl: './nav-buttons.component.html',
-  styleUrls: ['./nav-buttons.component.css']
+  styleUrls: ['./nav-buttons.component.scss']
 })
 export class NavButtonsComponent {
   public asfWebsiteUrl = 'https://www.asf.alaska.edu';
