c59684a262b00d0a821af44478ad72b5c1606098
William Horn <wbhorn@alaska.edu>
2019-06-11 15:03:13 -0800

    Remove extra code from image dialog component

--- src/app/components/bottom-menu/granule-detail/image-dialog/image-dialog.component.ts
+++ src/app/components/bottom-menu/granule-detail/image-dialog/image-dialog.component.ts
@@ -13,22 +13,13 @@ import * as models from '@models';
   templateUrl: './image-dialog.component.html',
   styleUrls: ['./image-dialog.component.scss']
 })
-export class ImageDialogComponent implements OnInit {
+export class ImageDialogComponent {
   public granule$ = this.store$.select(granulesStore.getSelectedGranule);
-  public granule: models.CMRProduct;
-
-  public granules$ = this.store$.select(granulesStore.getGranules);
-  public granules:  models.CMRProduct[];
 
   constructor(
     private store$: Store<AppState>,
     public dialogRef: MatDialogRef<ImageDialogComponent>
   ) { }
-
-  ngOnInit() {
-    this.granule$.subscribe(g => this.granule = g);
-    this.granules$.subscribe(gs => this.granules = gs);
-  }
 
   public onOpenImage(granule: models.CMRProduct) {
     window.open(granule.browse || 'assets/error.png');
