0e874f8a1f26f4ee2fd27f1d98fbf697f49f8e47
Gregory Short <glshort@alaska.edu>
2019-07-22 14:36:20 -0500

    Missed the .daac in the retirement URL

--- src/app/services/banner-api.service.ts
+++ src/app/services/banner-api.service.ts
@@ -14,7 +14,7 @@ export class BannerApiService {
   public load(): Observable<BannerApiResponse> {
     return of({
       banners: [{
-        text: "Looking for the old Vertex design? It will remain available <a href=\"https://vertex-retired.asf.alaska.edu\">here</a> until August 19, 2019",
+        text: "Looking for the old Vertex design? It will remain available <a href=\"https://vertex-retired.daac.asf.alaska.edu\">here</a> until August 19, 2019",
         type: 'message',
         target: [
           'sar-search'
