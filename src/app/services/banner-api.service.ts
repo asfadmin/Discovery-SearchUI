a0098333bfa733cf804ecb0b46a26c2048bf0efe
Mariya Shapran <mshapran@alaska.edu>
2019-07-26 12:08:49 -0700

    DS-2559: fixing space indentation

--- src/app/services/banner-api.service.ts
+++ src/app/services/banner-api.service.ts
@@ -14,7 +14,7 @@ export class BannerApiService {
   public load(): Observable<BannerApiResponse> {
     return of({
       banners: [
-       {
+        {
           text: '<p>Data availability will be intermittent due to system maintenance on August 4, 2019,'  +
           'starting at 16:00 (UTC) and ending 18:00 (UTC). We apologize for the inconvenience.' +
             '</p><p>' +
