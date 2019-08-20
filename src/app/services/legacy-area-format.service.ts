1e7b0690ea08900b6e2b81e5e216a9ec7db4e748
William Horn <wbhorn@alaska.edu>
2019-08-20 11:02:03 -0800

    Make only polygons with 3 or more points are valid for legacy vertex format

---
+++ src/app/services/legacy-area-format.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LegacyAreaFormatService {
+  constructor() { }
+
+  public isValid(numberList: string): boolean {
+    const numbers = this.parseNumberList(numberList);
+
+    return numbers.length >= 6 && numbers.length % 2 === 0;
+  }
+
+  public toWkt(numberList: string): string {
+    const numbers = this.parseNumberList(numberList);
+
+    const pairs = this.pairPoints(numbers);
+
+    const points = pairs
+    .reduce(
+      (wktPoints, [lon, lat]) => wktPoints + `${lon} ${lat},`, ''
+    ).slice(0, -1);
+
+    return `POLYGON((${points}))`;
+  }
+
+  private parseNumberList(polygon: string): number[] {
+    return polygon
+      .replace(/\s/g, '')
+      .split(',')
+      .map(num => +num)
+      .filter(num => !isNaN(num));
+  }
+
+  private pairPoints(numbers): number[][] {
+    const pairs = numbers.reduce((result, value, index, array) => {
+      if (index % 2 === 0) {
+        result.push(array.slice(index, index + 2));
+      }
+
+      return result;
+    }, []);
+
+    if (pairs[0] === pairs[pairs.length - 1]) {
+      pairs.pop();
+    }
+
+    return pairs;
+  }
+}
