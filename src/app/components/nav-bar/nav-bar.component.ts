6857c2d1cb729254897b6270b943f8681f9d9f5f
William Horn <wbhorn@alaska.edu>
2019-09-12 09:56:52 -0800

    Merge breadcrumb-list into nav-bar component
    Next step is to start splitting things out into components

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,195 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {
+  trigger, state, style, animate, transition
+} from '@angular/animations';
 
+import { Subject } from 'rxjs';
+import { tap, map, filter, delay } from 'rxjs/operators';
+import { Observable, combineLatest } from 'rxjs';
+import { Store, ActionsSubject } from '@ngrx/store';
+import { ofType } from '@ngrx/effects';
+import { ClipboardService } from 'ngx-clipboard';
+
+import { AppState } from '@store';
+import * as searchStore from '@store/search';
+import * as uiStore from '@store/ui';
+import * as queueStore from '@store/queue';
+
+import { MapService, WktService, LegacyAreaFormatService } from '@services';
+
+import { MatDialog } from '@angular/material/dialog';
+import { QueueComponent } from '@components/nav-bar/queue';
+
+import * as models from '@models/index';
+import * as filtersStore from '@store/filters';
+
+enum BreadcrumbFilterType {
+  SEARCH_TYPE = 'Search Type',
+  DATASET = 'Dataset',
+  DATE = 'Date',
+  AOI = 'Area of Interest',
+  PATH_FRAME = 'Path/Frame',
+  ADDITIONAL = 'Additional Filters',
+  FILTERS_MENU = '...',
+  NONE = 'None'
+}
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
+
+  animations: [
+    trigger('fadeTransition', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('100ms ease-in', style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('100ms ease-out', style({opacity: 0}))
+      ])
+    ])
+  ],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   @Input() isLoading: boolean;
+
+  @ViewChild('polygonForm', { static: false }) public polygonForm: NgForm;
+
+  public aoiErrors$ = new Subject<void>();
+  public isAOIError = false;
+  public isHoveringAOISelector = false;
+
+  public filterTypes = BreadcrumbFilterType;
+  public selectedFilter = BreadcrumbFilterType.NONE;
+  public searchType: models.SearchType = models.SearchType.DATASET;
+
+  public searchTypes = models.SearchType;
+  public polygon: string;
+
+  public queuedProducts$ = this.store$.select(queueStore.getQueuedProducts);
+
+  constructor(
+    private store$: Store<AppState>,
+    private actions$: ActionsSubject,
+    private legacyAreaFormat: LegacyAreaFormatService,
+    private mapService: MapService,
+    private wktService: WktService,
+    private dialog: MatDialog,
+    private clipboard: ClipboardService,
+  ) { }
+
+  ngOnInit() {
+    this.store$.select(uiStore.getSearchType).subscribe(
+      searchType => this.searchType = searchType
+    );
+
+    this.actions$.pipe(
+      ofType<searchStore.MakeSearch>(searchStore.SearchActionType.MAKE_SEARCH),
+    ).subscribe(
+      _ => this.onSearch()
+    );
+
+    const polygon$ = this.mapService.searchPolygon$;
+    polygon$.subscribe(
+      p => this.polygon = p
+    );
+
+    this.handleAOIErrors();
+  }
+
+  public onSearch(): void {
+    this.clearSelectedBreadcrumb();
+  }
+
+  public onClearSearch(): void {
+    this.store$.dispatch(new searchStore.ClearSearch());
+  }
+
+  public onOpenDownloadQueue(): void {
+    this.dialog.open(QueueComponent, {
+      id: 'dlQueueDialog',
+    });
+  }
+
+  public clearSelectedBreadcrumb(): void {
+    this.store$.dispatch(new uiStore.CloseFiltersMenu());
+    this.selectedFilter = BreadcrumbFilterType.NONE;
+  }
+
+  public onInputSearchPolygon(polygon: string): void {
+    if (this.legacyAreaFormat.isValid(polygon)) {
+      polygon = this.legacyAreaFormat.toWkt(polygon);
+    }
+
+    this.loadWKT(polygon);
+  }
+
+  private productType$() {
+    return this.store$.select(filtersStore.getProductTypes).pipe(
+      map(types => types.map(type => type.apiValue)),
+      map(
+        types => Array.from(new Set(types))
+          .join(',')
+      ),
+      map(types => ({ processinglevel: types }))
+    );
+  }
+
+  private loadWKT(polygon: string): void {
+    try {
+      const features = this.wktService.wktToFeature(
+        polygon,
+        this.mapService.epsg()
+      );
+
+      this.mapService.setDrawFeature(features);
+    } catch (e) {
+      this.aoiErrors$.next();
+    }
+  }
+
+  public onNewSelectedFilter(filterType: BreadcrumbFilterType): void {
+    this.selectedFilter = this.selectedFilter === filterType ?
+      BreadcrumbFilterType.NONE : filterType;
+
+    this.store$.dispatch(new uiStore.CloseFiltersMenu());
+  }
+
+  public onToggleFiltersMenu(): void {
+    this.store$.dispatch(new uiStore.ToggleFiltersMenu());
+    this.selectedFilter = BreadcrumbFilterType.NONE;
+  }
+
+  public onSetSearchType(searchType: models.SearchType): void {
+    this.clearSelectedBreadcrumb();
+    this.store$.dispatch(new searchStore.ClearSearch());
+    this.store$.dispatch(new uiStore.SetSearchType(searchType));
+  }
+
+  public onCopy(): void {
+    this.clipboard.copyFromContent(this.polygon);
+  }
+
+  private handleAOIErrors(): void {
+    this.aoiErrors$.pipe(
+      tap(_ => {
+        this.isAOIError = true;
+        this.mapService.clearDrawLayer();
+        this.polygonForm.reset();
+        this.polygonForm.form
+          .controls['searchPolygon']
+          .setErrors({'incorrect': true});
+      }),
+      delay(820),
+    ).subscribe(_ => {
+      this.isAOIError = false;
+      this.polygonForm.form
+        .controls['searchPolygon']
+        .setErrors(null);
+    });
+  }
 }
+
--- src/app/components/nav-bar/nav-bar.module.ts
+++ src/app/components/nav-bar/nav-bar.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatBadgeModule } from '@angular/material/badge';
@@ -14,24 +16,56 @@ import { NavBarComponent } from './nav-bar.component';
 import { QueueModule, QueueComponent } from './queue';
 import { BreadcrumbListModule } from './breadcrumb-list';
 import { NavButtonsModule } from './nav-buttons';
+import { InfoBarComponent } from './info-bar/info-bar.component';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+
+import { DatasetSelectorModule } from '@components/shared/selectors/dataset-selector';
+import { DateSelectorModule } from '@components/shared/selectors/date-selector';
+import { AoiOptionsModule } from '@components/shared/aoi-options';
+import { MaxResultsSelectorModule } from '@components/shared/max-results-selector';
+import { SearchButtonModule } from '@components/shared/search-button';
+import { ClearButtonModule } from '@components/shared/clear-button';
+
+import { SearchSelectorModule } from '@components/shared/selectors/search-selector';
+import { LogoModule } from '@components/nav-bar/logo/logo.module';
+
+import { CustomBreakPointsProvider } from '@services/custom-breakpoints.ts';
 
 @NgModule({
   declarations: [
     NavBarComponent,
+    InfoBarComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
+    FlexLayoutModule,
+
     MatToolbarModule,
     MatProgressBarModule,
     MatBadgeModule,
     MatMenuModule,
     MatInputModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatDatepickerModule,
     MatSharedModule,
-    QueueModule,
 
+    QueueModule,
     BreadcrumbListModule ,
+
+    SearchSelectorModule,
+    DatasetSelectorModule,
+    DateSelectorModule,
+    AoiOptionsModule,
+    MaxResultsSelectorModule,
+    SearchButtonModule,
+    ClearButtonModule,
     NavButtonsModule,
+    LogoModule,
   ],
   entryComponents: [
     QueueComponent
