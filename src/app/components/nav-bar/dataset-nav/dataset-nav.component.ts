bcc7962370c735872a9192fb4d844415e6a29382
William Horn <wbhorn@alaska.edu>
2019-10-17 11:29:51 -0800

    DS-2699 Hide date filters and aoi as screen gets smaller

--- src/app/components/nav-bar/dataset-nav/dataset-nav.component.ts
+++ src/app/components/nav-bar/dataset-nav/dataset-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { ofType } from '@ngrx/effects';
 
@@ -9,6 +10,13 @@ import * as uiStore from '@store/ui';
 import * as queueStore from '@store/queue';
 
 import * as models from '@models';
+import * as services from '@services';
+
+enum Breakpoints {
+  FULL = 3,
+  MEDIUM = 2,
+  SMALL = 1,
+}
 
 @Component({
   selector: 'app-dataset-nav',
@@ -19,9 +27,22 @@ export class DatasetNavComponent {
   @Output() public openQueue = new EventEmitter<void>();
 
   public queuedProducts$ = this.store$.select(queueStore.getQueuedProducts);
+  public breakpoint$ = this.screenSize.size$.pipe(
+    map(({ width, height }) => {
+      if (width > 1440) {
+        return Breakpoints.FULL;
+      } else if (width > 1140) {
+        return Breakpoints.MEDIUM;
+      } else {
+        return Breakpoints.SMALL;
+      }
+    })
+  );
+  public breakpoints = Breakpoints;
 
   constructor(
     private store$: Store<AppState>,
+    private screenSize: services.ScreenSizeService
   ) { }
 
   public onToggleFiltersMenu(): void {
