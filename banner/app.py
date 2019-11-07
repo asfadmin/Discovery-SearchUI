6811c8fee8a39211680f09532a125355277399bf
William Horn <wbhorn@alaska.edu>
2019-11-06 21:01:09 -0900

    Only add events that are current as banners

--- banner/app.py
+++ banner/app.py
@@ -3,28 +3,44 @@ from datetime import datetime
 import requests
 import pytz
 from ics import Calendar
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-CALENDAR_URL = 'https://calendar.google.com/calendar/ical/alaska.edu_mhaq7n1h4ljoonprpbvgct8rv0%40group.calendar.google.com/public/basic.ics'
 
+@app.route('/calendar/<deployment>')
+def banners(deployment):
+    calendar = calendar_url(deployment)
+    c = Calendar(requests.get(calendar).text)
 
-@app.route('/calendar')
-def index():
-    c = Calendar(requests.get(CALENDAR_URL).text)
     tz = pytz.timezone('US/Alaska')
-    currentTime = datetime.now(tz)
+    current = tz.localize(datetime.now())
     banners = []
 
-    print('current', currentTime)
-    print('YYYY-MM-DD[*HH[:MM[:SS[.fff[fff]]]][+HH:MM[:SS[.ffffff]]]]')
     for e in c.events:
-        start = datetime.fromisoformat(str(e.begin))
-        end = datetime.fromisoformat(str(e.end))
-        print(start, end)
+        start, end = [
+            tz.localize(load_date_from_str(str(d))) for d in [e.begin, e.end]
+        ]
 
-    return 'hello'
+        if current >= start and current <= end:
+            banners.append({
+                'name': e.name
+            })
+
+    return jsonify(banners)
+
+
+def load_date_from_str(date_str):
+    # 2019-11-06T18:30:00+00:00
+
+    return datetime.strptime(date_str.split('+')[0], '%Y-%m-%dT%H:%M:%S')
+
+
+def calendar_url(deployment):
+    if 'test' in deployment:
+        return 'https://calendar.google.com/calendar/ical/alaska.edu_bl352sdorvboe8mmglnivrd8go%40group.calendar.google.com/public/basic.ics'
+    else:
+        return 'https://calendar.google.com/calendar/ical/alaska.edu_mhaq7n1h4ljoonprpbvgct8rv0%40group.calendar.google.com/public/basic.ics'
 
 
 if __name__ == "__main__":
