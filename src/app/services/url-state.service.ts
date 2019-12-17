57d5200e53cc9f80610f32edea44f221df8e0800
William Horn <wbhorn@alaska.edu>
2019-12-17 11:50:57 -0900

    Fix function name

--- src/app/services/url-state.service.ts
+++ src/app/services/url-state.service.ts
@@ -265,7 +265,7 @@ export class UrlStateService {
     }, {
       name: 'center',
       source: this.mapService.center$.pipe(
-        map(({ lon, lat }) => ({ lon: this.with7Decimals(lon), lat: this.with7Decimals(lat) })),
+        map(({ lon, lat }) => ({ lon: this.limitDecimals(lon), lat: this.limitDecimals(lat) })),
         map(({ lon, lat }) => ({ center: `${lon},${lat}` }))
       ),
       loader: this.loadMapCenter
@@ -556,7 +556,7 @@ export class UrlStateService {
   private clamp = (value: number, min: number, max: number) =>
     Math.min(Math.max(value, min), max)
 
-  private with6Decimals(num: number) {
+  private limitDecimals(num: number) {
     return num.toString().match(/^-?\d+(?:\.\d{0,6})?/)[0];
   }
 }
