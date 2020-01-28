0b48a26f1d52cf6a2d9b7d7e71783c0a68d8cdd3
William Horn <wbhorn@alaska.edu>
2020-01-28 12:45:22 -0900

    Remove /test from api-test url

--- src/assets/envs/env-test.js
+++ src/assets/envs/env-test.js
@@ -2,7 +2,7 @@
   window._env = {
     api: {
       prod: 'https://api.daac.asf.alaska.edu',
-      test: 'https://api-test.asf.alaska.edu/test',
+      test: 'https://api-test.asf.alaska.edu',
     },
     auth: {
       api: {
