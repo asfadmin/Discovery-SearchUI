5b77ec8d5ae8d07bc53cf1d649fec4e652f62317
William Horn <wbhorn@alaska.edu>
2020-02-19 13:33:05 -0900

    Remove default value from unzip api

--- src/app/services/unzip-api.service.ts
+++ src/app/services/unzip-api.service.ts
@@ -29,92 +29,11 @@ export class UnzipApiService {
   }
 
   public unzip(downloadUrl: string): void {
-    const productUnzipUrl = `${downloadUrl.replace('datapool', 'unzip')}/`;
+    const productUnzipUrl = downloadUrl.replace('datapool', 'unzip');
 
     console.log(productUnzipUrl);
-    this.http.get(productUnzipUrl, { withCredentials: true }).pipe(
-      catchError(val => of(this.exampleResp()))
-    ).subscribe(
+    this.http.get(productUnzipUrl, { withCredentials: true }).subscribe(
       console.log
     );
-  }
-
-  private exampleResp(): UnzippedFolder {
-    return {
-      "contents": [
-        {
-          "contents": [
-            {
-              "name": "AP_27803_PLR_F0910_RT2.dem.tif",
-              "size": 355859,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.dem.tif"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.ls_map.tif",
-              "size": 196943,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.ls_map.tif"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.kmz",
-              "size": 5991338,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.kmz"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.iso.xml",
-              "size": 101123,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.iso.xml"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.geo.jpg",
-              "size": 602666,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.geo.jpg"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.geo.jpg.aux.xml",
-              "size": 953,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.geo.jpg.aux.xml"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2.geo.wld",
-              "size": 92,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2.geo.wld"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2_HH.tif",
-              "size": 9419521,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2_HH.tif"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2_HV.tif",
-              "size": 9323701,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2_HV.tif"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2_VH.tif",
-              "size": 9378537,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2_VH.tif"
-            },
-            {
-              "name": "AP_27803_PLR_F0910_RT2_VV.tif",
-              "size": 9358131,
-              "type": "file",
-              "url": "https://unzip-test.asf.alaska.edu/RTC_LOW_RES/A3/AP_27803_PLR_F0910_RT2.zip/AP_27803_PLR_F0910_RT2/AP_27803_PLR_F0910_RT2_VV.tif"
-            }
-          ],
-          "name": "AP_27803_PLR_F0910_RT2",
-          "type": "dir"
-        }
-      ]
-    };
   }
 }
