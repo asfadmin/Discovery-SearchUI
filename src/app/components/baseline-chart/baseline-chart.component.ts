4e71bbf19bca8126d44d472d0365ee6d64e31502
William Horn <wbhorn@alaska.edu>
2020-03-02 11:49:34 -0900

    Setup default Chart.js scatter chart

--- src/app/components/baseline-chart/baseline-chart.component.ts
+++ src/app/components/baseline-chart/baseline-chart.component.ts
@@ -18,37 +18,24 @@ export class BaselineChartComponent implements OnInit {
 
   private initChart() {
     const myChart = new Chart(this.baselineChart.nativeElement, {
-      type: 'bar',
+      type: 'scatter',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
+          label: 'Scatter Dataset',
+          data: [
+            { x: -10, y: 0 },
+            { x: 0, y: 10 },
+            { x: 10, y: 5 },
+            { x: 10, y: 20 },
+            { x: 15, y: 10 }
+          ]
         }]
       },
       options: {
         scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom'
           }]
         }
       }
