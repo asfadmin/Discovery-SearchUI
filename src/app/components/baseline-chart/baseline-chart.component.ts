6c04d7fce29b4112fac89cc7e1f413814f254788
William Horn <wbhorn@alaska.edu>
2020-03-04 12:43:37 -0900

    Update tooltip of points to have labels

--- src/app/components/baseline-chart/baseline-chart.component.ts
+++ src/app/components/baseline-chart/baseline-chart.component.ts
@@ -57,136 +57,144 @@ export class BaselineChartComponent implements OnInit {
       });
   }
 
-private criticalBaselineDataset(points: {x: number, y: number}[]) {
-  const { min, max } = this.determineMinMax(points);
+  private criticalBaselineDataset(points: {x: number, y: number}[]) {
+    const { min, max } = this.determineMinMax(points);
 
-  const buffer = (max.x - min.x) * .25;
+    const buffer = (max.x - min.x) * .25;
 
-  const minDataset = [
-    {x: min.x - buffer - 10, y: -this.criticalBaseline},
-    {x: max.x + buffer + 10, y: -this.criticalBaseline}
-  ];
+    const minDataset = [
+      {x: min.x - buffer - 10, y: -this.criticalBaseline},
+      {x: max.x + buffer + 10, y: -this.criticalBaseline}
+    ];
 
-  const maxDataset = [
-    {x: min.x - buffer - 10, y: this.criticalBaseline},
-    {x: max.x + buffer + 10, y: this.criticalBaseline}
-  ];
+    const maxDataset = [
+      {x: min.x - buffer - 10, y: this.criticalBaseline},
+      {x: max.x + buffer + 10, y: this.criticalBaseline}
+    ];
 
-  return { minDataset, maxDataset };
-}
+    return { minDataset, maxDataset };
+  }
 
-private determineMinMax(points: {x: number, y: number}[]) {
-  const min = { x: 0, y: 0 };
-  const max = { x: 0, y: 0 };
+  private determineMinMax(points: {x: number, y: number}[]) {
+    const min = { x: 0, y: 0 };
+    const max = { x: 0, y: 0 };
 
-  points.map(point => {
-    if (point.x < min.x) {
-      min.x = point.x;
-    }
-    if (point.y < min.y) {
-      min.y = point.y;
-    }
-    if (point.x > max.x) {
-      max.x = point.x;
-    }
-    if (point.y > max.y) {
-      max.y = point.y;
-    }
-  });
+    points.map(point => {
+      if (point.x < min.x) {
+        min.x = point.x;
+      }
+      if (point.y < min.y) {
+        min.y = point.y;
+      }
+      if (point.x > max.x) {
+        max.x = point.x;
+      }
+      if (point.y > max.y) {
+        max.y = point.y;
+      }
+    });
 
-  return { min, max };
-}
+    return { min, max };
+  }
 
-private initChart() {
-  const styles = this.chartStyles();
+  private initChart() {
+    const styles = this.chartStyles();
 
-  this.chart = new Chart(this.baselineChart.nativeElement, {
-    type: 'scatter',
-    data: {
-      datasets: [{
-        label: 'Within Critical Baseline',
-        data: [],
-        ...styles.secondary
-      }, {
-        label: 'Min Critical Baseline',
-        showLine: true,
-        type: 'line',
-        data: [],
-        ...styles.critical
-      }, {
-        label: 'Max Critical Baseline',
-        showLine: true,
-        type: 'line',
-        data: [],
-        ...styles.critical
-      }]
-    },
-    options: {
-      responsive: true,
-      zoom: {
-        enabled: true,
-        mode: 'xy',
-        rangeMin: { x: null, y: null },
-        rangeMax: { x: null, y: null }
-      },
-      pan: {
-        enabled: true,
-        mode: 'xy',
-        rangeMin: { x: null, y: null },
-        rangeMax: { x: null, y: null }
-      },
-      scales: {
-        xAxes: [{
-          type: 'linear',
-          position: 'bottom',
-          scaleLabel: {
-            display: true,
-            labelString: 'Temporal (days)'
-          }
-        }],
-        yAxes: [{
-          type: 'linear',
-          position: 'left',
-          scaleLabel: {
-            display: true,
-            labelString: 'Perpendicular (m)'
-          },
+    this.chart = new Chart(this.baselineChart.nativeElement, {
+      type: 'scatter',
+      data: {
+        datasets: [{
+          label: 'Within Critical Baseline',
+          data: [],
+          ...styles.secondary
+        }, {
+          label: 'Min Critical Baseline',
+          showLine: true,
+          type: 'line',
+          data: [],
+          ...styles.critical
+        }, {
+          label: 'Max Critical Baseline',
+          showLine: true,
+          type: 'line',
+          data: [],
+          ...styles.critical
         }]
       },
-      legend: {
-        labels: {
-          filter: (legendItem, chartData) => {
-            return (legendItem.datasetIndex < 1);
+      options: {
+        responsive: true,
+        zoom: {
+          enabled: true,
+          mode: 'xy',
+          rangeMin: { x: null, y: null },
+          rangeMax: { x: null, y: null }
+        },
+        pan: {
+          enabled: true,
+          mode: 'xy',
+          rangeMin: { x: null, y: null },
+          rangeMax: { x: null, y: null }
+        },
+        scales: {
+          xAxes: [{
+            type: 'linear',
+            position: 'bottom',
+            scaleLabel: {
+              display: true,
+              labelString: 'Temporal (days)'
+            }
+          }],
+          yAxes: [{
+            type: 'linear',
+            position: 'left',
+            scaleLabel: {
+              display: true,
+              labelString: 'Perpendicular (m)'
+            },
+          }]
+        },
+        legend: {
+          labels: {
+            filter: (legendItem, chartData) => {
+              return (legendItem.datasetIndex < 1);
+            }
           }
-        }
-      },
-      chartArea: {
-        backgroundColor: 'rgba(0, 0, 0, 0)'
-      },
-      animation: false,
-      elements: {point: {radius: 6, hoverRadius: 9}},
-      hoverMode: 'single',
-    }}
-  );
-}
+        },
+        chartArea: {
+          backgroundColor: 'rgba(0, 0, 0, 0)'
+        },
+        animation: false,
+        elements: {point: {radius: 6, hoverRadius: 9}},
+        hoverMode: 'single',
+        tooltips: {
+          backgroundColor: 'lightgrey',
+          bodyFontColor: 'black',
+          callbacks: {
+            label: (tooltipItem, data) =>
+              ` ${tooltipItem.xLabel} days, ${tooltipItem.yLabel} meters`
+          }
+        },
+      }}
+    );
+  }
 
-private chartStyles() {
-  return {
-    critical: {
-      pointStyle: 'dash',
-      backgroundColor: 'rgba(0,0,0,0.05)',
-      borderColor: 'white',
-      borderWidth: 1,
-      fill: 'origin',
-    },
-    secondary: {
-      pointBackgroundColor: 'grey',
-      pointBorderColor: 'white',
-      borderWidth: 1,
-      radius: 5,
-      pointHoverBackgroundColor: 'grey',
-      pointHoverBorderColor: 'black'
-    }
-  };
-}
+  private chartStyles() {
+    return {
+      critical: {
+        pointStyle: 'dash',
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderColor: 'white',
+        borderWidth: 1,
+        fill: 'origin',
+      },
+      secondary: {
+        pointBackgroundColor: 'grey',
+        pointBorderColor: 'white',
+        borderWidth: 1,
+        radius: 5,
+        pointHoverBackgroundColor: 'grey',
+        pointHoverBorderColor: 'black'
+      }
+    };
+  }
 }
