33874b636b4454c9984df76cf5f1177de2c061f7
William Horn <wbhorn@alaska.edu>
2020-03-26 05:34:32 -0800

    In baseline dropdown use start and end date

--- src/app/services/scenes.service.ts
+++ src/app/services/scenes.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 
 import { Observable, combineLatest } from 'rxjs';
 import { map, tap, filter, } from 'rxjs/operators';
+import * as moment from 'moment';
 
 import { Store } from '@ngrx/store';
 import { AppState } from '@store/app.reducer';
 import { getAllProducts, getScenes } from '@store/scenes/scenes.reducer';
-import { getTemporalRange, getPerpendicularRange } from '@store/filters/filters.reducer';
+import { getTemporalRange, getPerpendicularRange, getDateRange } from '@store/filters/filters.reducer';
 import { getSearchType } from '@store/search/search.reducer';
 
 import { criticalBaselineFor, CMRProduct, SearchType, Range } from '@models';
@@ -37,13 +38,15 @@ export class ScenesService {
       products,
       this.store$.select(getTemporalRange),
       this.store$.select(getPerpendicularRange),
-      this.store$.select(getSearchType)
+      this.store$.select(getDateRange),
+      this.store$.select(getSearchType),
     ).pipe(
-      map(([scenes, tempRange, perpRange, searchType]) => {
+      map(([scenes, tempRange, perpRange, dateRange, searchType]) => {
         return (searchType === SearchType.BASELINE) ?
           scenes.filter(scene =>
             this.valInRange(scene.metadata.temporal, tempRange) &&
-            this.valInRange(scene.metadata.perpendicular, perpRange)
+            this.valInRange(scene.metadata.perpendicular, perpRange) &&
+            this.dateInRange(scene.metadata.date, dateRange)
           ) :
           scenes;
       })
@@ -55,6 +58,27 @@ export class ScenesService {
       Number.isNaN(range.start) || range.start === null ||
       Number.isNaN(range.end) || range.end === null ||
       (val >= range.start && val <= range.end)
+    );
+  }
+
+  private dateInRange(date: moment.Moment, range: Range<Date | null>): boolean {
+    return (
+      this.after(date, range.start) &&
+      this.before(date, range.end)
+    );
+  }
+
+  private after(check: moment.Moment, pivot: Date | null): boolean {
+    return (
+      pivot === null ||
+      check.isAfter(moment.utc(pivot))
+    );
+  }
+
+  private before(check: moment.Moment, pivot: Date | null): boolean {
+    return (
+      pivot === null ||
+      check.isBefore(moment.utc(pivot))
     );
   }
 }
