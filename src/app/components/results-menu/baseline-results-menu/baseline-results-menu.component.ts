21b888485abe0e24add03b249d7670f2537e2f5d
William Horn <wbhorn@alaska.edu>
2020-04-07 04:57:08 -0800

    Work on stacking scene list and detail for baseline medium screens

--- src/app/components/results-menu/baseline-results-menu/baseline-results-menu.component.ts
+++ src/app/components/results-menu/baseline-results-menu/baseline-results-menu.component.ts
@@ -6,17 +6,34 @@ import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import * as scenesStore from '@store/scenes';
 
+import { Breakpoints } from '@models';
+import { ScreenSizeService } from '@services';
+
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-baseline-results-menu',
   templateUrl: './baseline-results-menu.component.html',
   styleUrls: ['./baseline-results-menu.component.scss',  '../results-menu.component.scss']
 })
-export class BaselineResultsMenuComponent implements OnInit {
+export class BaselineResultsMenuComponent implements OnInit, OnDestroy {
   @Input() resize$: Observable<void>;
 
-  constructor() { }
+  public breakpoint: Breakpoints;
+  public breakpoints = Breakpoints;
+  private subs = new SubSink();
+
+  constructor(private screenSize: ScreenSizeService) { }
 
   ngOnInit(): void {
+    this.subs.add(
+      this.screenSize.breakpoint$.subscribe(
+        point => this.breakpoint = point
+      )
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
