b8aab3ca9a1e566cf55dbab04eea3f39d38f8566
William Horn <wbhorn@alaska.edu>
2020-04-08 07:12:57 -0800

    Improve formatting of baseline results on medium screens

--- src/app/components/results-menu/baseline-results-menu/baseline-results-menu.component.ts
+++ src/app/components/results-menu/baseline-results-menu/baseline-results-menu.component.ts
@@ -11,6 +11,11 @@ import { ScreenSizeService } from '@services';
 
 import { SubSink } from 'subsink';
 
+enum CardViews {
+  LIST = 0,
+  DETAIL = 1
+}
+
 @Component({
   selector: 'app-baseline-results-menu',
   templateUrl: './baseline-results-menu.component.html',
@@ -18,6 +23,9 @@ import { SubSink } from 'subsink';
 })
 export class BaselineResultsMenuComponent implements OnInit, OnDestroy {
   @Input() resize$: Observable<void>;
+
+  public view = CardViews.LIST;
+  public Views = CardViews;
 
   public breakpoint: Breakpoints;
   public breakpoints = Breakpoints;
@@ -31,6 +39,14 @@ export class BaselineResultsMenuComponent implements OnInit, OnDestroy {
         point => this.breakpoint = point
       )
     );
+  }
+
+  public onSelectList(): void {
+    this.view = CardViews.LIST;
+  }
+
+  public onSelectDetail(): void {
+    this.view = CardViews.DETAIL;
   }
 
   ngOnDestroy() {
