d6bb7866c3edd7e2742abeb62102b5381e559424
William Horn <wbhorn@alaska.edu>
2020-04-30 11:35:58 -0800

    Fix more links

--- src/app/models/datasets/sirc.ts
+++ src/app/models/datasets/sirc.ts
@@ -18,7 +18,7 @@ export const sirc = {
     end: new Date('1994/10/11 17:02:09 UTC')
   },
   infoUrl: 'https://eospso.nasa.gov/missions/spaceborne-imaging-radar-c',
-  citationUrl: 'https://asf.alaska.edu/data-sets/sar-data-sets/sentinel-1/sentinel-1-how-to-cite/',
+  citationUrl: 'https://asf.alaska.edu/information/how-to-cite-data/',
   frequency: 'C-Band and L-Band',
   source: {
     name: 'NASA',
