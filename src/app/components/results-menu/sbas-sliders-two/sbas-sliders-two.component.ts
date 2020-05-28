ec6a79b1ed3372e41b0e39ecafea78a96d9c3c2c
Andrew Anderson <raanderson9@alaska.edu>
2020-05-28 11:59:19 -0700

    SBAS Slider WIP

---
+++ src/app/components/results-menu/sbas-sliders-two/sbas-sliders-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sbas-sliders-two',
+  templateUrl: './sbas-sliders-two.component.html',
+  styleUrls: ['./sbas-sliders-two.component.scss']
+})
+export class SbasSlidersTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
