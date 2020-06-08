dd98294f0558e2726af5cd2814cbdaf229afe8e3
William Horn <wbhorn@alaska.edu>
2020-06-08 07:11:56 -0800

    First draft of integrating zoom/panning back into sbas chart

--- src/app/components/sbas-chart/sbas-chart.component.ts
+++ src/app/components/sbas-chart/sbas-chart.component.ts
@@ -41,6 +41,7 @@ export class SBASChartComponent implements OnInit, OnDestroy {
   private yAxis;
   private chart;
   private scatter;
+  private line;
   private pairs;
 
   private selected: CMRProduct;
@@ -75,8 +76,8 @@ export class SBASChartComponent implements OnInit, OnDestroy {
 
   private makeSbasChart() {
     const margin = {top: 9, right: 30, bottom: 30, left: 60},
-        width = 760 - margin.left - margin.right,
-        height = 450 - margin.top - margin.bottom;
+        width = 800 - margin.left - margin.right,
+        height = 420 - margin.top - margin.bottom;
 
     this.chart = d3.select('#sbasChart')
       .append('svg')
@@ -103,101 +104,104 @@ export class SBASChartComponent implements OnInit, OnDestroy {
       this.scenes.map(s => s.metadata.perpendicular)
     );
 
-      // Add Y axis
-      this.y = d3.scaleLinear()
-        .domain(yExtent)
-        .range([ height, 0]);
-      this.yAxis = this.chart.append('g')
-        .call(d3.axisLeft(this.y));
+    // Add Y axis
+    this.y = d3.scaleLinear()
+      .domain(yExtent)
+      .range([ height, 0]);
+    this.yAxis = this.chart.append('g')
+      .call(d3.axisLeft(this.y));
 
-      // Add a clipPath: everything out of this area won't be drawn.
-      const clip = this.chart.append('defs').append('SVG:clipPath')
-          .attr('id', 'clip')
-          .append('SVG:rect')
-          .attr('width', width )
-          .attr('height', height )
-          .attr('x', 0)
-          .attr('y', 0);
+    // Create the scatter variable: where both the circles and the brush take place
+    this.scatter = this.chart.append('g')
+      .attr('clip-path', 'url(#clip)');
 
-      // Create the scatter variable: where both the circles and the brush take place
-      this.scatter = this.chart.append('g')
-        .attr('clip-path', 'url(#clip)');
+    const zoom = d3.zoom()
+        .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
+        .extent([[0, 0], [width, height]])
+        .on('zoom', _ => this.updateChart());
 
-      // Add circles
-      this.scatter.append('g')
-        .selectAll('circle')
-        .data(this.scenes)
-        .enter()
-        .append('circle')
-          .attr('cx', (d: CMRProduct) => this.x(d.metadata.temporal) )
-          .attr('cy', (d: CMRProduct) => this.y(d.metadata.perpendicular) )
-          .attr('r', 8)
-          .style('fill', '#61a3a9')
-          .style('opacity', 0.5);
+    this.scatter.append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .style('fill', 'transparent')
+        .style('pointer-events', 'all')
+        .call(zoom);
 
-      const line = d3.line()
-          .x((product: any) => <any>this.x(product.metadata.temporal))
-          .y((product: any) => this.y(product.metadata.perpendicular));
+    // Add circles
+    this.scatter.append('g')
+      .selectAll('circle')
+      .data(this.scenes)
+      .enter()
+      .append('circle')
+        .attr('cx', (d: CMRProduct) => this.x(d.metadata.temporal) )
+        .attr('cy', (d: CMRProduct) => this.y(d.metadata.perpendicular) )
+        .attr('r', 8)
+        .style('fill', '#61a3a9')
+        .style('opacity', 0.5);
 
-      const lines = this.scatter.append('g')
-          .attr('fill', 'none')
-          .attr('stroke-linejoin', 'round')
-          .attr('stroke-linecap', 'round')
-        .selectAll('path');
+    this.line = d3.line()
+        .x((product: any) => this.x(product.metadata.temporal))
+        .y((product: any) => this.y(product.metadata.perpendicular));
 
-      const self = this;
-      lines
-        .data(this.pairs)
-        .join('path')
-          .style('mix-blend-mode', 'multiply')
-          .attr('stroke', 'steelblue')
-          .attr('stroke-width', 3)
-          .attr('cursor', 'pointer')
-          .attr('d', pair => line(pair))
-          .on('mouseover', function(d) {
-            self.setHovered(d, d3.select(this));
-          })
-          .on('mouseleave', function(d) {
-            self.clearHovered();
-          })
-          .on('click', p => this.setSelected(p, line));
+    const lines = this.scatter.append('g')
+        .attr('fill', 'none')
+        .attr('stroke-linejoin', 'round')
+        .attr('stroke-linecap', 'round')
+      .selectAll('path');
 
-      const updateChart = () => {
-        // recover the new scale
-        const newX = d3.event.transform.rescaleX(this.x);
-        const newY = d3.event.transform.rescaleY(this.y);
+    const self = this;
+    lines
+      .data(this.pairs)
+      .join('path')
+        .style('mix-blend-mode', 'multiply')
+        .attr('class', 'base-line')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 3)
+        .attr('cursor', 'pointer')
+        .attr('d', pair => this.line(pair))
+        .on('mouseover', function(d) {
+          self.setHovered(d, d3.select(this));
+        })
+        .on('mouseleave', function(d) {
+          self.clearHovered();
+        })
+        .on('click', p => this.setSelected(p));
 
-        // update axes with these new boundaries
-        this.xAxis.call(d3.axisBottom(newX));
-        this.yAxis.call(d3.axisLeft(newY));
+    // Add a clipPath: everything out of this area won't be drawn.
+    const clip = this.chart.append('defs').append('SVG:clipPath')
+        .attr('id', 'clip')
+        .append('SVG:rect')
+        .attr('width', width )
+        .attr('height', height )
+        .attr('x', 0)
+        .attr('y', 0);
+  }
 
-        // update circle position
-        this.scatter
-          .selectAll('circle')
-            .attr('cx', (d: CMRProduct) => newX(d.metadata.temporal) )
-            .attr('cy', (d: CMRProduct) => newY(d.metadata.perpendicular) );
+  private updateChart() {
+    // recover the new scale
+    const newX = d3.event.transform.rescaleX(this.x);
+    const newY = d3.event.transform.rescaleY(this.y);
 
-        const newLine = d3.line()
-            .x((product: any) => newX(product.metadata.temporal))
-            .y((product: any) => newY(product.metadata.perpendicular));
+    // update axes with these new boundaries
+    this.xAxis.call(d3.axisBottom(newX));
+    this.yAxis.call(d3.axisLeft(newY));
 
-        this.scatter.selectAll('path')
-          .attr('d', pair => newLine(pair));
-      };
+    // update circle position
+    this.scatter
+      .selectAll('circle')
+        .attr('cx', (d: CMRProduct) => newX(d.metadata.temporal) )
+        .attr('cy', (d: CMRProduct) => newY(d.metadata.perpendicular) );
 
-      /*
-      const zoom = d3.zoom()
-          .scaleExtent([.5, 20])  // This control how much you can unzoom (x0.5) and zoom (x20)
-          .extent([[0, 0], [width, height]])
-          .on('zoom', updateChart);
+    this.line = d3.line()
+        .x((product: any) => newX(product.metadata.temporal))
+        .y((product: any) => newY(product.metadata.perpendicular));
 
-      this.chart.append('rect')
-          .attr('width', width)
-          .attr('height', height)
-          .style('fill', 'none')
-          .style('pointer-events', 'auto')
-          .call(zoom);
-      */
+    this.scatter.selectAll('.base-line')
+      .attr('d', pair => this.line(pair));
+
+    if (this.selectedPair[0] !== null && this.selectedPair[1] !== null) {
+      this.setSelected(this.selectedPair);
+    }
   }
 
   private setHovered(pair, newHovered) {
@@ -212,7 +216,6 @@ export class SBASChartComponent implements OnInit, OnDestroy {
 
     newHovered
       .style('mix-blend-mode', 'normal')
-      .attr('class', 'hovered-line')
       .attr('stroke', 'red')
       .attr('stroke-width', 5);
 
@@ -228,7 +231,7 @@ export class SBASChartComponent implements OnInit, OnDestroy {
       .attr('stroke-width', 3);
   }
 
-  private setSelected(pair, line) {
+  public setSelected = (pair) => {
     this.selectedPair = pair;
 
     this.scatter.select('.selected-line').remove();
@@ -238,7 +241,7 @@ export class SBASChartComponent implements OnInit, OnDestroy {
       .attr('stroke', 'orange')
       .attr('stroke-width', 3)
       .attr('cursor', 'pointer')
-      .attr('d', _ => line(pair));
+      .attr('d', _ => this.line(pair));
 
   }
 
