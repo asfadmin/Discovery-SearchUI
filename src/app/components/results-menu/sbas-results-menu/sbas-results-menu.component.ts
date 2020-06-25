d13e7022235e876b791d94f29ee54db4007d9565
Andrew Anderson <raanderson9@alaska.edu>
2020-06-24 19:35:31 -0700

    WIP SBAS Chart left edge draggable. Needs visual UI control but it functions.

--- src/app/components/results-menu/sbas-results-menu/sbas-results-menu.component.ts
+++ src/app/components/results-menu/sbas-results-menu/sbas-results-menu.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import {Component, OnInit, Input, OnDestroy, ViewChild, ElementRef} from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { ResizeEvent } from 'angular-resizable-element';
 
 import { Store } from '@ngrx/store';
 import { AppState } from '@store';
@@ -24,11 +25,18 @@ enum CardViews {
   styleUrls: ['./sbas-results-menu.component.scss',  '../results-menu.component.scss']
 })
 export class SBASResultsMenuComponent implements OnInit, OnDestroy {
+
+  @ViewChild('listCard', {read: ElementRef}) listCardView: ElementRef;
+  @ViewChild('chartCard', {read: ElementRef}) chartCardView: ElementRef;
+
   @Input() resize$: Observable<void>;
   public pair: CMRProductPair;
 
   public view = CardViews.LIST;
   public Views = CardViews;
+
+  public listCardMaxWidth = '38%';
+  public chartCardMaxWidth = '55%';
 
   public breakpoint: Breakpoints;
   public breakpoints = Breakpoints;
@@ -53,6 +61,37 @@ export class SBASResultsMenuComponent implements OnInit, OnDestroy {
         (selected: CMRProductPair) => this.pair = selected
       )
     );
+  }
+
+  public onResizeEnd(event: ResizeEvent): void {
+    console.log('Element was resized', event);
+    console.log('listCard fxFlex:', this.listCardView.nativeElement);
+    console.log('chartCard fxFlex:', this.chartCardView.nativeElement);
+    const windowWidth = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth;
+    const newChartWidth = event.rectangle.width;
+    const newChartMaxWidth = Math.round((newChartWidth / windowWidth) * 100);
+    const newListMaxWidth = 100 - newChartMaxWidth;
+
+    this.listCardMaxWidth = newListMaxWidth.toString() + '%';
+    this.chartCardMaxWidth = newChartMaxWidth.toString() + '%';
+    console.log('windowWidth:', windowWidth);
+    console.log('newChartWidth:', newChartWidth);
+    console.log('newChartMaxWidth:', newChartMaxWidth);
+    console.log('newListMaxWidth:', newListMaxWidth);
+    console.log('event.rectangle.width:', event.rectangle.width);
+    console.log('event.edges.left:', Number(event.edges.left));
+    console.log('listCardMaxWidth:', this.listCardMaxWidth);
+    console.log('chartCardMaxWidth:', this.chartCardMaxWidth);
+
+    // event.id.style = {
+    //   position: 'fixed',
+    //   left: `${event.rectangle.left}px`,
+    //   top: `${event.rectangle.top}px`,
+    //   width: `${event.rectangle.width}px`,
+    //   height: `${event.rectangle.height}px`
+    // };
   }
 
   public onToggleFiltersMenu(): void {
