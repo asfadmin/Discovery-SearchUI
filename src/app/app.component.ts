f84497483d24d7ffd5e3da1c056aeb637b983d65
kim <jdfairbanks@alaska.edu>
2021-02-03 17:35:06 -0900

    added add and remove on demand icons for add scene to on demand button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -159,6 +159,14 @@ export class AppComponent implements OnInit, OnDestroy {
       'hyp3',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/hyp3.svg')
     );
+    this.matIconRegistry.addSvgIcon(
+      'add_on_demand',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/add_on_demand.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'remove_on_demand',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/remove_on_demand.svg')
+    );
   }
 
   private loadProductQueue(): void {
