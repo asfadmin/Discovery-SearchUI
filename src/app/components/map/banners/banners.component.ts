2e3943d6633af6a34bdfbbf48db25624b719ab60
Andrew Anderson <raanderson9@alaska.edu>
2021-04-06 15:37:50 -0700

    WIP

--- src/app/components/map/banners/banners.component.ts
+++ src/app/components/map/banners/banners.component.ts
@@ -1,9 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, Directive} from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import {
   trigger, style, animate, transition
 } from '@angular/animations';
 
 import { Banner } from '@models';
+
+// tslint:disable-next-line:directive-selector
+@Directive({selector: '[bannerCreate]'})
+export class BannerCreateDirective implements OnInit {
+  @Input() bannerCreate: Banner;
+
+  constructor( private toastr: ToastrService ) {
+  }
+
+  ngOnInit(): void {
+    let warningFlag = false;
+
+    if (this.bannerCreate.name.toLowerCase().indexOf('outage') > -1||
+      this.bannerCreate.name.toLowerCase().indexOf('network')  > -1||
+      this.bannerCreate.name.toLowerCase().indexOf('problem') > -1
+    ) { warningFlag = true; }
+
+    if (warningFlag) {
+      this.toastr.warning( this.bannerCreate.text, this.bannerCreate.name, {
+        enableHtml: true,
+        closeButton: true,
+        disableTimeOut: true,
+      });
+    } else {
+      this.toastr.success( this.bannerCreate.text, this.bannerCreate.name, {
+        enableHtml: true,
+        closeButton: true,
+        timeOut: 10000,
+      });
+    }
+  }
+
+}
 
 @Component({
   selector: 'app-banners',
@@ -25,9 +59,14 @@ export class BannersComponent implements OnInit {
   @Input() banners: Banner[];
   @Output() removeBanner = new EventEmitter<Banner>();
 
-  constructor() { }
+  constructor( ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  public processBannerText ( banner: Banner) {
+    return banner.text;
   }
 
   public remove(banner: Banner): void {
