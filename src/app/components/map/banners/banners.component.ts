41227aae7fec80dca93ca68736d76c2f8ac6653c
Andrew Anderson <raanderson9@alaska.edu>
2021-04-07 12:00:06 -0700

    WIP on Calendar & Toastr.

--- src/app/components/map/banners/banners.component.ts
+++ src/app/components/map/banners/banners.component.ts
@@ -1,9 +1,5 @@
 import {Component, OnInit, Input, Output, EventEmitter, Directive} from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import {
-  trigger, style, animate, transition
-} from '@angular/animations';
-
 import { Banner } from '@models';
 
 // tslint:disable-next-line:directive-selector
@@ -15,58 +11,27 @@ export class BannerCreateDirective implements OnInit {
   }
 
   ngOnInit(): void {
-    let warningFlag = false;
-
-    if (this.bannerCreate.name.toLowerCase().indexOf('outage') > -1||
-      this.bannerCreate.name.toLowerCase().indexOf('network')  > -1||
-      this.bannerCreate.name.toLowerCase().indexOf('problem') > -1
-    ) { warningFlag = true; }
-
-    if (warningFlag) {
-      this.toastr.warning( this.bannerCreate.text, this.bannerCreate.name, {
-        enableHtml: true,
-        closeButton: true,
-        disableTimeOut: true,
-      });
-    } else {
-      this.toastr.success( this.bannerCreate.text, this.bannerCreate.name, {
-        enableHtml: true,
-        closeButton: true,
-        timeOut: 10000,
-      });
-    }
+    this.toastr.warning(this.bannerCreate.text, this.bannerCreate.name, {
+      enableHtml: true,
+      closeButton: true,
+      disableTimeOut: true,
+    });
   }
-
 }
 
 @Component({
   selector: 'app-banners',
   templateUrl: './banners.component.html',
   styleUrls: ['./banners.component.scss'],
-  animations: [
-    trigger('bannerTransition', [
-      transition(':enter', [
-        style({transform: 'translateY(-100%)'}),
-        animate('200ms ease-in', style({transform: 'translateX(0%)'}))
-      ]),
-      transition(':leave', [
-        animate('200ms ease-out', style({transform: 'translateX(-100%)'}))
-      ])
-    ])
-  ],
 })
 export class BannersComponent implements OnInit {
   @Input() banners: Banner[];
   @Output() removeBanner = new EventEmitter<Banner>();
 
-  constructor( ) {
+  constructor() {
   }
 
   ngOnInit(): void {
-  }
-
-  public processBannerText ( banner: Banner) {
-    return banner.text;
   }
 
   public remove(banner: Banner): void {
