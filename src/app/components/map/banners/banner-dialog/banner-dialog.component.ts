7c08f038b4d6d6ce3babd0801b33bdd39f65048d
codefactor-io <support@codefactor.io>
2021-04-13 22:43:06 +0000

    [CodeFactor] Apply fixes

--- src/app/components/map/banners/banner-dialog/banner-dialog.component.ts
+++ src/app/components/map/banners/banner-dialog/banner-dialog.component.ts
@@ -8,7 +8,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./banner-dialog.component.scss']
 })
 export class BannerDialogComponent implements OnInit {
-  htmlContent: string
+  htmlContent: string;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData ) { }
