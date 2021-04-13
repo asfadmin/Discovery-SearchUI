65b67d8b68f8c8e675097dce43b1f3ff4d08f017
Andrew Anderson <raanderson9@alaska.edu>
2021-04-13 12:16:37 -0700

    WIP on banner dialog

--- src/app/components/map/banners/banners.component.ts
+++ src/app/components/map/banners/banners.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter, Directive } from '@angular/core';
+import { Component, OnInit, Input, Directive } from '@angular/core';
 import { ActiveToast, Toast, ToastrService } from 'ngx-toastr';
 import { Banner } from '@models';
+import { BannerDialogComponent} from '@components/map/banners/banner-dialog/banner-dialog.component';
 
 // tslint:disable-next-line:directive-selector
 @Directive({selector: '[bannerCreate]'})
 export class BannerCreateDirective implements OnInit {
   @Input() bannerCreate: Banner;
-  constructor( private toastr: ToastrService ) {
-  }
+  constructor( private toastr: ToastrService,
+               public dialog: BannerDialogComponent,
+               ) {}
+
+  public toastRef;
 
   ngOnInit(): void {
-    const text: string = this.bannerCreate.text;
-    const toast: ActiveToast<any> = this.toastr.info(text, this.bannerCreate.name, {
+    const msg: string = this.bannerCreate.text;
+    const title: string = this.bannerCreate.name;
+    const toast: ActiveToast<any> = this.toastr.info(msg, title, {
       enableHtml: true,
       closeButton: true,
       disableTimeOut: true,
@@ -19,7 +24,14 @@ export class BannerCreateDirective implements OnInit {
     });
 
     const toastComponent: Toast = toast.toastRef.componentInstance;
-    toastComponent.message = text;
+    toastComponent.message = msg;
+
+    toast.onTap.subscribe(x => {
+      this.dialog.openDialog();
+      // toastComponent.message = msg + '\nthis is the full message';
+      // alert(`<b>${title}</b>\n ${msg}`);
+      console.log(x);
+    });
   }
 
   public myFunction() {
@@ -34,7 +46,6 @@ export class BannerCreateDirective implements OnInit {
 })
 export class BannersComponent implements OnInit {
   @Input() banners: Banner[];
-  @Output() removeBanner = new EventEmitter<Banner>();
 
   constructor() {
   }
@@ -42,7 +53,4 @@ export class BannersComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public remove(banner: Banner): void {
-    this.removeBanner.emit(banner);
-  }
 }
