b1c80294d4cda97b370a4ab6bdb29f33fb17f95d
kim <jdfairbanks@alaska.edu>
2021-04-15 16:01:29 -0800

    adds auto suggestion to On Demand product name filter

--- src/app/components/shared/selectors/job-product-name-selector/job-product-name-selector.component.ts
+++ src/app/components/shared/selectors/job-product-name-selector/job-product-name-selector.component.ts
@@ -4,6 +4,11 @@ import { SubSink } from 'subsink';
 import * as filtersStore from '@store/filters';
 import { AppState } from '@store';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { ScenesService } from '@services';
+import { getScenes } from '@store/scenes';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-job-product-name-selector',
@@ -14,9 +19,14 @@ export class JobProductNameSelectorComponent implements OnInit, OnDestroy {
   @Input() headerView: boolean;
   public productNameFilter: string;
   private subs = new SubSink();
+  public filteredOptionsList: string[];
+  public unfilteredScenes: string[];
+
+  public myControl = new FormControl();
 
   constructor(
-    private store$: Store<AppState>
+    private store$: Store<AppState>,
+    private scenesService: ScenesService,
   ) { }
 
   ngOnInit(): void {
@@ -25,11 +35,74 @@ export class JobProductNameSelectorComponent implements OnInit, OnDestroy {
         productNameFilter => this.productNameFilter = productNameFilter
       )
     );
+
+    const fileNames = this.scenesService.scenes$().pipe(
+      map(scenes =>
+          scenes.map(scene => scene.metadata.fileName.toLowerCase().split('.')[0])
+      )
+    );
+
+    this.subs.add(
+      this.store$.select(getScenes).subscribe(
+        res => this.unfilteredScenes = res.map(scene => scene.metadata.fileName.toLowerCase().split('.')[0])
+      )
+    );
+
+    this.subs.add(
+      combineLatest([this.myControl.valueChanges, fileNames]).pipe(
+        map(([_, filteredRes]) => filteredRes)
+      ).subscribe(res => {
+          if (this.productNameFilter != null) {
+            const temp = this.productNameFilter.replace(/\s+/g, '').endsWith(',')
+              ? this.unfilteredScenes.filter(scene => !res.includes(scene)) : res;
+            this.filteredOptionsList = temp.filter(file => this.autoSuggestion(file.toLowerCase()));
+          } else {
+            this.filteredOptionsList = this.unfilteredScenes;
+          }
+        }
+      )
+    );
   }
 
   public onFilterProductName(productName: string): void {
     const action = new filtersStore.SetProductNameFilter(productName);
     this.store$.dispatch(action);
+  }
+
+  public autoCompleteEntry(suggestion: string) {
+    let output = '';
+
+    if(this.productNameFilter != null) {
+    if (this.productNameFilter.split(',').length > 1) {
+      if (this.productNameFilter.replace(/\s+/g, '').endsWith(',')) {
+        output = this.productNameFilter + suggestion;
+      } else {
+        const fields = this.productNameFilter.split(',');
+        fields[fields.length - 1] = suggestion;
+        output = fields.join(', ');
+      }
+    } else {
+      output = suggestion;
+    }
+  }
+    return output;
+  }
+
+  public autoSuggestion(suggestion: string) {
+    if (this.productNameFilter == null) {
+      return suggestion;
+    }
+    if (this.productNameFilter.split(',').length > 1) {
+      const filenames = this.productNameFilter.replace(/\s+/g, '').toLowerCase();
+      if (filenames.endsWith(',')) {
+        return filenames.includes(suggestion) ? '' : suggestion;
+      } else {
+        const filenamesList = filenames.split(',');
+        const lastEntry = filenamesList[filenamesList.length - 1];
+        return suggestion.includes(lastEntry) ? suggestion : '';
+      }
+    }
+    return suggestion.includes(this.productNameFilter) ? suggestion : '';
   }
 
   ngOnDestroy(): void {
--- src/app/components/shared/selectors/job-product-name-selector/job-product-name-selector.module.ts
+++ src/app/components/shared/selectors/job-product-name-selector/job-product-name-selector.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatInputModule} from '@angular/material/input'; 
-import { JobProductNameSelectorComponent } from './job-product-name-selector.component'
-
+import {MatInputModule} from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { JobProductNameSelectorComponent } from './job-product-name-selector.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [JobProductNameSelectorComponent],
   imports: [
     CommonModule,
-    MatInputModule
+    MatAutocompleteModule,
+    MatInputModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     JobProductNameSelectorComponent
