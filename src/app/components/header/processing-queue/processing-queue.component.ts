b7df2ab971746d7e5fa9a3de0644143ef9ef3c34
tylercchase <tylercchase@gmail.com>
2021-06-08 08:56:20 -0800

    format correctly

--- src/app/components/header/processing-queue/processing-queue.component.ts
+++ src/app/components/header/processing-queue/processing-queue.component.ts
@@ -260,9 +260,9 @@ export class ProcessingQueueComponent implements OnInit {
         const jobText = hyp3JobsBatch.length > 1 ? `${hyp3JobsBatch.length} jobs` : 'Job';
 
         this.notificationService.info(`${jobText} processing, click to open your jobs.`, 'Jobs Submitted', {
-      closeButton: true,
-      disableTimeOut: true,
-    }).onTap.subscribe(() => {
+          closeButton: true,
+          disableTimeOut: true,
+        }).onTap.subscribe(() => {
           const searchType = models.SearchType.CUSTOM_PRODUCTS;
           this.store$.dispatch(new searchStore.SetSearchType(searchType));
         });
