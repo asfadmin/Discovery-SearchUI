690de1e72546641dadce29170c48aff0fde16a4b
kim <jdfairbanks@alaska.edu>
2021-06-11 16:33:36 -0800

    cleaned up some lines, add preset button now disabled when user hasn't entered a valid name for it

--- src/app/components/shared/save-user-filters/save-user-filters.component.ts
+++ src/app/components/shared/save-user-filters/save-user-filters.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FilterType, SearchType } from '@models';
 import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import * as userStore from '@store/user';
 import * as filterStore from '@store/filters';
 import * as searchStore from '@store/search';
-import { filter, tap } from 'rxjs/operators';
-// import { FiltersState } from '@store/filters';
-// import { GeographicFiltersType, ListFiltersType, SbasFiltersType, BaselineFiltersType } from '@models';
+import { filter } from 'rxjs/operators';
 import { SubSink } from 'subsink';
 import { combineLatest } from 'rxjs';
 @Component({
@@ -15,7 +13,7 @@ import { combineLatest } from 'rxjs';
   templateUrl: './save-user-filters.component.html',
   styleUrls: ['./save-user-filters.component.scss']
 })
-export class SaveUserFiltersComponent implements OnInit {
+export class SaveUserFiltersComponent implements OnInit, OnDestroy {
 
   private currentFilters: {name: string, searchType: SearchType, filter: FilterType}[] = [];
 
@@ -24,15 +22,13 @@ export class SaveUserFiltersComponent implements OnInit {
   private currentFiltersBySearchType = {};
 
   public currentSearchType: SearchType;
-  public newFilterName: string;
+  public newFilterName = '';
 
   private subs = new SubSink();
 
   constructor(private store$: Store<AppState>){}
 
   ngOnInit(): void {
-    // this.store$.select(userStore.getSearchHistory).pipe(
-    //   map(searches => searches[0])).subscribe(latest_search => this.currentSearch = latest_search)
 
     this.subs.add(
       this.store$.select(userStore.getSavedFilters).pipe(
@@ -64,21 +60,17 @@ export class SaveUserFiltersComponent implements OnInit {
 
   }
 
-  public onSaveFilters() {
-    // const currentFilterName = (this.currentFilters.length + 1).toString();
-    if(this.newFilterName === undefined) {
-      return;
-    }
-    if(this.newFilterName === '') {
-      return;
-    }
-    if(this.currentFilters.map(preset => preset.name).includes(this.newFilterName)) {
-      return;
-    }
-    this.store$.dispatch(new userStore.AddNewFiltersPreset({name: this.newFilterName, filter: this.currentFiltersBySearchType[this.currentSearchType], searchType: this.currentSearchType}));
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 
+  public onSaveFilters() {
+    this.store$.dispatch(new userStore.AddNewFiltersPreset({name: this.newFilterName, filter: this.currentFiltersBySearchType[this.currentSearchType], searchType: this.currentSearchType}));
     this.newFilterName = '';
-    // this.userFilters.push((this.userFilters.length - 1).toString());
+  }
+
+  public isValidPresetName(name: string) {
+    return name !== '' && !this.currentFilters.map(preset => preset.name).includes(name);
   }
 
   public filterBySearchType(filters: {name: string, searchType: SearchType, filter: FilterType}[]) {
