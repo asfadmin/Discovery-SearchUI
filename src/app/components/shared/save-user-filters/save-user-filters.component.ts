6f9e3fccd75de9a35de26e8a90085caeb96d4ae5
codefactor-io <support@codefactor.io>
2021-06-30 18:34:31 +0000

    [CodeFactor] Apply fixes to commit 66ceb69

--- src/app/components/shared/save-user-filters/save-user-filters.component.ts
+++ src/app/components/shared/save-user-filters/save-user-filters.component.ts
@@ -40,7 +40,7 @@ export class SaveUserFiltersComponent implements OnInit, OnDestroy {
   private subs = new SubSink();
 
   constructor(private store$: Store<AppState>,
-    private screenSize: ScreenSizeService){}
+    private screenSize: ScreenSizeService) {}
 
   ngOnInit(): void {
 
@@ -51,8 +51,7 @@ export class SaveUserFiltersComponent implements OnInit, OnDestroy {
     );
 
     this.subs.add(
-      this.store$.select(userStore.getSavedFilters).subscribe ( userFilters =>
-        {
+      this.store$.select(userStore.getSavedFilters).subscribe ( userFilters => {
           this.userFilters = userFilters;
           const output = this.filterBySearchType(this.userFilters);
           this.displayedFilter = output;
@@ -60,8 +59,7 @@ export class SaveUserFiltersComponent implements OnInit, OnDestroy {
     );
 
     this.subs.add(
-      this.store$.select(searchStore.getSearchType).subscribe(searchtype =>
-        {
+      this.store$.select(searchStore.getSearchType).subscribe(searchtype => {
           this.currentSearchType = searchtype;
           const output = this.filterBySearchType(this.userFilters);
           this.displayedFilter = output;
@@ -89,7 +87,7 @@ export class SaveUserFiltersComponent implements OnInit, OnDestroy {
       ).subscribe(
         _ => {
           if (this.saveFilterOn) {
-            this.newFilterName = 'New Filter Preset'
+            this.newFilterName = 'New Filter Preset';
             this.onSaveFilters();
             this.store$.dispatch(new uiStore.SetSaveFilterOn(false));
           }
@@ -110,7 +108,7 @@ export class SaveUserFiltersComponent implements OnInit, OnDestroy {
   }
 
   public filterBySearchType(filters: {name: string, searchType: SearchType, filters: FilterType}[]) {
-    let output = filters.filter(preset => preset.searchType === this.currentSearchType);
+    const output = filters.filter(preset => preset.searchType === this.currentSearchType);
     return output;
   }
 
