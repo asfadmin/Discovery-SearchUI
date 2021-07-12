486e6595bd29453a0c1838fe1d61f48e781cbd5e
kim <jdfairbanks@alaska.edu>
2021-07-12 14:35:51 -0800

    adds file name import support for list searches with Geojson and CSV imports. Disables it on kml and metalink imports

--- src/app/components/filters-dropdown/list-filters/list-filters.component.ts
+++ src/app/components/filters-dropdown/list-filters/list-filters.component.ts
@@ -47,6 +47,7 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
 
   public searchList: string;
   public listSearchMode$ = this.store$.select(filtersStore.getListSearchMode);
+  private listSearchMode: ListSearchType;
   private newListInput$ = new Subject<string | null>();
   public breakpoint$ = this.screenSize.breakpoint$;
   public breakpoints = models.Breakpoints;
@@ -99,6 +100,12 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
       ).subscribe(
         _ => _
       )
+
+
+    );
+
+    this.subs.add(
+      this.listSearchMode$.subscribe(mode => this.listSearchMode = mode)
     );
 
     this.subs.add(
@@ -225,7 +232,16 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
   private parseCSV(file) {
     this.ngxCsvParser.parse(file, { header: true, delimiter: ',' })
     .pipe(first()).subscribe((result: Array<any>) => {
-      const granules: string[] = result.map(row => row['Granule Name']);
+      const granules: string[] = result.map(row =>
+        {
+          let processingType ='';
+
+          if(this.listSearchMode === ListSearchType.PRODUCT) {
+            processingType = `-{row['Processing Level']}`;
+          }
+
+          return row['Granule Name'] + processingType
+        });
       this.updateSearchList(granules);
     }, (error: NgxCSVParserError) => {
       console.log('Error', error);
@@ -237,7 +253,8 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
       filereader.onload = _ => {
         const res = filereader.result as string;
         const features: any[] = JSON.parse(res)['features'];
-        const granules = features.map(feature => feature['properties']['fileID']);
+        const typeKey = this.listSearchMode === ListSearchType.PRODUCT ? 'fileID' : 'sceneName'
+        const granules = features.map(feature => feature['properties'][typeKey]);
 
         this.updateSearchList(granules);
     };
@@ -245,23 +262,26 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
   }
 
   private parseKML(file) {
-    const filereader = new FileReader();
-      filereader.onload = _ => {
-        const res = filereader.result as string;
-        const parser = new xml2js.Parser({ explicitArray: false });
-        const observable = from (parser.parseStringPromise(res));
+    if(this.listSearchMode === ListSearchType.SCENE) {
+      const filereader = new FileReader();
+        filereader.onload = _ => {
+          const res = filereader.result as string;
+          const parser = new xml2js.Parser({ explicitArray: false });
+          const observable = from (parser.parseStringPromise(res));
 
-        observable.pipe(first()).subscribe(result => {
-            const placemarks: [] = result['kml']['Document']['Placemark'];
-            const granules: string[] = placemarks.map(placemark => placemark['name']);
+          observable.pipe(first()).subscribe(result => {
+              const placemarks: [] = result['kml']['Document']['Placemark'];
+              const granules: string[] = placemarks.map(placemark => placemark['name']);
 
-            this.updateSearchList(granules);
-          });
-    };
-    filereader.readAsText(file);
+              this.updateSearchList(granules);
+            });
+      };
+      filereader.readAsText(file);
+  }
   }
 
   private parseMetalink(file) {
+    if(this.listSearchMode === ListSearchType.SCENE) {
     const filereader = new FileReader();
       filereader.onload = _ => {
         const res = filereader.result as string;
@@ -276,6 +296,7 @@ export class ListFiltersComponent implements OnInit, OnDestroy {
           });
     };
     filereader.readAsText(file);
+  }
   }
 
   private updateSearchList(granules: string[]) {
