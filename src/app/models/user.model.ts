40c1d3278cfde705a4eca79d451099f2d035ba3a
Andrew Anderson <raanderson9@alaska.edu>
2021-07-29 14:45:47 -0700

    WIP on reading download chunk size from user preferences.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -11,6 +11,7 @@ export interface UserProfile {
   defaultDataset: string;
   mapLayer: MapLayerTypes;
   maxResults: number;
+  defaultMaxConcurrentDownloads;
   defaultFilterPresets: {
     'Baseline Search': string,
     'Geographic Search': string,
