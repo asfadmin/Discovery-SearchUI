b4dee3d85320b05c02433408db57b53bd5d0440b
Andrew Anderson <raanderson9@alaska.edu>
2021-08-05 15:02:46 -0700

    WIP download button in lots of places now

--- src/app/components/header/queue/download-file-button/download-file-button.component.ts
+++ src/app/components/header/queue/download-file-button/download-file-button.component.ts
@@ -10,11 +10,14 @@ import { CMRProduct } from '@models';
 })
 export class DownloadFileButtonComponent implements OnInit {
   @Input() product: CMRProduct;
+  @Input() href: string;
   @Output()
   productDownloaded: EventEmitter<CMRProduct> = new EventEmitter<CMRProduct>();
   public dFile: Download;
   public dlInProgress = false;
   public dlComplete = false;
+  public url: string;
+  public fileName: string;
 
   constructor(
     private downloadService: DownloadService,
@@ -23,19 +26,28 @@ export class DownloadFileButtonComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public downloadFile(product: CMRProduct) {
+  public downloadFile(product: CMRProduct, href?: string) {
     if (this.dlInProgress) {
       return;
     }
+
     this.dlInProgress = true;
-    const url = product.downloadUrl;
-    const fileName = product.file;
-    this.downloadService.download(url, fileName).subscribe(resp => {
+
+    // if an href is passed then the product ignored
+    if (href) {
+      this.url = href;
+      this.fileName = this.url.substring(this.url.lastIndexOf('/') + 1);
+    } else {
+      this.url = product.downloadUrl;
+      this.fileName = product.file;
+    }
+
+    this.downloadService.download(this.url, this.fileName).subscribe(resp => {
       this.dFile = resp;
       if (resp.state === 'DONE') {
         this.dlInProgress = false;
         this.dlComplete = true;
-        this.productDownloaded.emit(product);
+        this.productDownloaded.emit( product );
       }
     });
   }
