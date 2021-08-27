ad157a9ad981bc3513ebbd7ba6ad40697a9bee3c
kim <jdfairbanks@alaska.edu>
2021-08-27 15:16:14 -0800

    latest values for sarviews event types filter pulled from filter  store

--- src/app/components/shared/selectors/sarviews-event-type-selector/sarviews-event-type-selector.component.ts
+++ src/app/components/shared/selectors/sarviews-event-type-selector/sarviews-event-type-selector.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import { SarviewsEventType } from '@models';
 import { MatSelectChange } from '@angular/material/select';
 
 import * as filterStore from '@store/filters';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-sarviews-event-type-selector',
   templateUrl: './sarviews-event-type-selector.component.html',
   styleUrls: ['./sarviews-event-type-selector.component.scss']
 })
-export class SarviewsEventTypeSelectorComponent implements OnInit {
+export class SarviewsEventTypeSelectorComponent implements OnInit, OnDestroy {
 
   constructor(private store$: Store<AppState>) { }
 
+  public currentEventTypes$ = this.store$.select(filterStore.getSarviewsEventTypes);
   public eventTypes = [SarviewsEventType.FLOOD, SarviewsEventType.QUAKE, SarviewsEventType.VOLCANO];
+
+  public selectedTypesList: string[] = [];
+
+  public subs = new SubSink();
 
   public onNewEventTypes(event: MatSelectChange) {
     const selectedEventNames = (event.value as string[]);
@@ -25,6 +31,14 @@ export class SarviewsEventTypeSelectorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.subs.add(
+      this.currentEventTypes$.subscribe(
+        types => this.selectedTypesList = types.map(t => t.valueOf())
+      )
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subs.unsubscribe;
+  }
 }
