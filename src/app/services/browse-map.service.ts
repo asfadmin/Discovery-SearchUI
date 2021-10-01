a062ad25758b150c293601edf582de0e525adc7d
kim <jdfairbanks@alaska.edu>
2021-10-01 12:10:10 -0800

    user can now click on pinned products to bring them to the front

--- src/app/services/browse-map.service.ts
+++ src/app/services/browse-map.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import Map from 'ol/Map.js';
+import { Map } from 'ol';
 import View from 'ol/View.js';
 // import { Extent } from 'ol/extent.js';
 // import ImageLayer from 'ol/layer/Image.js';
@@ -22,6 +22,8 @@ import VectorSource from 'ol/source/Vector';
 import WKT from 'ol/format/WKT';
 import LayerGroup from 'ol/layer/Group';
 import { Collection } from 'ol';
+// import { click } from 'ol/events/condition';
+// import { Select } from 'ol/interaction';
 // import { Feature } from 'ol';
 // import { transformExtent } from 'ol/proj';
 
@@ -44,6 +46,12 @@ export class BrowseMapService {
   private browseLayer: ImageLayer;
   private view: View;
   private pinnedProducts: LayerGroup;
+
+  // private selectClick = new Select({
+  //   condition: click,
+  //   style: null,
+  //   // layers: l => !!this.pinnedProducts.getLayersArray().find(pinned => pinned.get("product_id") === l?.get("product_id")),
+  // });
 
   public setBrowse(browse: string, dim: Dimension, wkt: string = ''): void {
     const format = new WKT();
@@ -97,6 +105,69 @@ export class BrowseMapService {
       this.pinnedProducts = new LayerGroup();
       this.map = this.newMap(this.view, [map_layer, imagePolygonLayer, Imagelayer]);
       this.map.addLayer(this.pinnedProducts);
+
+      // this.selectClick.setActive(true);
+
+      // this.pinnedProducts.getLayers().inter
+      // this.map.addInteraction(this.selectClick);
+
+
+      // this.selectClick.on('select', e => {
+      //   console.log((e))
+      // });
+
+      this.map.on("singleclick", e => {
+        // const f = this.map.getfeat(e.pixel, {
+        //   layerFilter: (l) => l.getClassName() === "product_pin"
+        // });
+        // console.log(f);
+        this.map.forEachLayerAtPixel(e.pixel, l => {
+          console.log(e, l.getClassName());
+          console.log(l.get("product_id"));
+
+          this.pinnedProducts.getLayers().remove(l);
+          this.pinnedProducts.getLayers().push(l);
+          // this.pinnedProducts.getLayersArray().slice()
+          // l.setZIndex(this.pinnedProducts.getLayers().getLength() + 1);
+          return true;
+        })
+      })
+
+    //   this.map.on("singleclick", (evt) => {
+    //     this.map.forEachFeatureAtPixel(evt.pixel, (_, l) => {
+    //       // if(!!this.pinnedProducts.getLayers().getArray().includes(l)) {
+
+    //       if(this.pinnedProducts.getLayers().getArray().includes(l)) {
+    //         // let removed = (this.pinnedProducts.getLayers() as Collection<ImageLayer>).remove(l);
+    //         // let pinnedCopy = this.pinnedProducts.getLayers();
+    //         // pinnedCopy.push(removed);
+    //         this.pinnedProducts.getLayers().remove(l);
+    //         this.pinnedProducts.getLayers().push(l);
+    //         // this.pinnedProducts.getLayers().clear();
+    //         // this.pinnedProducts.setLayers(pinnedCopy);
+    //         // const found = this.pinnedProducts.getLayers().getArray().find(
+    //         //   (temp: ImageLayer) => {
+    //         //     temp.get("product_id") === l.get("product_id")
+    //         //   }
+    //         // );
+
+
+    //         // l.setZIndex(this.pinnedProducts.getLayers().getLength() + 1);
+    //         // l.getZIndex()
+    //         // this.pinnedProducts.getLayersArray().find(l => l)
+    //         // console.log(l.getZIndex());
+    //         // this.pinnedProducts.getLayers().remove(found);
+    //         // this.pinnedProducts.getLayers().push(found);
+    //         // l.setZIndex(this.pinnedProducts.getLayers().getLength())
+    //         // console.log(l.getZIndex());
+    //         return true;
+    //       }
+    //     }
+    //     );
+    //   });
+    //   // this.map.on('click', function(event) {
+
+    //   //   this.map
     }
 
     this.browseLayer = Imagelayer;
@@ -106,7 +177,7 @@ export class BrowseMapService {
     this.browseLayer.setOpacity(opacity);
   }
 
-  public createImageLayer(url: string, wkt: string) {
+  public createImageLayer(url: string, wkt: string, className: string = "ol-layer") {
     const format = new WKT();
     const feature = format.readFeature(wkt, {dataProjection: 'EPSG:4326',
     featureProjection: 'EPSG:3857'});
@@ -117,6 +188,9 @@ export class BrowseMapService {
         url,
         imageExtent: polygon.getExtent(),
       }),
+      className,
+      zIndex: 0,
+      extent: polygon.getExtent(),
       opacity: this.browseLayer?.getOpacity() ?? 1.0
     });
 
@@ -144,22 +218,28 @@ export class BrowseMapService {
 
     const imageLayers = pinned_ids.reduce((prev, product_id) => {
       let current = prev;
-      const pinnedProd = this.createImageLayer(pinned[product_id].url, pinned[product_id].wkt);
-      pinnedProd.set("product_id", product_id)
+      const pinnedProd = this.createImageLayer(pinned[product_id].url, pinned[product_id].wkt, "product_pin");
+      pinnedProd.set("product_id", product_id);
       current.push(pinnedProd);
       return current;
     }, new Collection<ImageLayer>());
 
     imageLayers.forEach( l => {
+      // this.map.addLayer(l);
         this.pinnedProducts.getLayers().push(l);
       }
     );
   }
 
   private unpinProducts(product_ids: string[]) {
-    this.pinnedProducts.getLayers().forEach(
+    if(product_ids.length === 0) {
+      this.pinnedProducts.getLayers().clear();
+      return;
+    }
+    this.pinnedProducts.getLayers()?.forEach(
       l => {
-        if(product_ids.includes(l.get("product_id"))) {
+        if(product_ids.includes(l?.get("product_id"))) {
+          // this.map.removeLayer(l);
           this.pinnedProducts.getLayers().remove(l);
         }
       }
