e8a7f0acb4f6c7f848149425165920262b32338e
kim <jdfairbanks@alaska.edu>
2021-10-22 14:16:42 -0800

    adds settings icon to event dates in event-metadata, with option to set start and end dates

--- src/app/components/shared/event-metadata/event-metadata.component.ts
+++ src/app/components/shared/event-metadata/event-metadata.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import * as models from '@models';
+import { Store } from '@ngrx/store';
+import { AppState } from '@store';
+import { SetEndDate, SetStartDate } from '@store/filters';
+import * as moment from 'moment';
+// import { Moment } from 'moment';
 
 @Component({
   selector: 'app-event-metadata',
@@ -10,9 +15,19 @@ export class EventMetadataComponent implements OnInit {
   @Input() event: models.SarviewsEvent;
   @Input() eventType: models.SarviewsEventType;
   public eventTypes = models.SarviewsEventType;
-  constructor() { }
+
+  constructor(private store$: Store<AppState>) { }
 
   ngOnInit(): void {
   }
 
+  public onSetStartDate(date: Date) {
+    const startOf = moment(date).startOf('day');
+    const startDate = startOf.toDate();
+    this.store$.dispatch(new SetStartDate(startDate));
+  }
+
+  public onSetEndDate(date: Date) {
+    this.store$.dispatch(new SetEndDate(date));
+  }
 }
--- src/app/components/shared/event-metadata/event-metadata.module.ts
+++ src/app/components/shared/event-metadata/event-metadata.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventMetadataComponent } from './event-metadata.component';
 import { PipesModule } from '@pipes';
-
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     EventMetadataComponent
   ],
   imports: [
     CommonModule,
-    PipesModule
+    PipesModule,
+    MatMenuModule,
+    MatIconModule
   ],
   exports: [
     EventMetadataComponent
