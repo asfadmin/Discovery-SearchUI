ed799a1606cdda73f7a63c64cb4c7409f91ceaf9
kim <jdfairbanks@alaska.edu>
2021-11-05 16:59:18 -0800

    adds cors headers for raster image layer

--- src/app/services/browse-overlay.service.ts
+++ src/app/services/browse-overlay.service.ts
@@ -6,7 +6,9 @@ import Geometry from 'ol/geom/Geometry';
 import Polygon from 'ol/geom/Polygon';
 import ImageLayer from 'ol/layer/Image';
 import Static from 'ol/source/ImageStatic';
-import Raster, { RasterOperationType } from 'ol/source/Raster';
+import Raster
+// , { RasterOperationType }
+from 'ol/source/Raster';
 
 import { Coordinate } from 'ol/coordinate';
 import MultiPolygon from 'ol/geom/MultiPolygon';
@@ -27,7 +29,7 @@ export class BrowseOverlayService {
        });
   }
 
-  public createImageLayer(url: string, wkt: string, className: string = 'ol-layer', layer_id: string = '') {
+  public createNormalImageLayer(url: string, wkt: string, className: string = 'ol-layer', layer_id: string = '') {
     const feature = this.wktService.wktToFeature(wkt, 'EPSG:3857');
     const polygon = this.getPolygonFromFeature(feature, wkt);
     const source = this.createImageSource(url, polygon.getExtent());
@@ -54,7 +56,7 @@ export class BrowseOverlayService {
     return polygon;
   }
 
-  public createRasterImageLayer(url: string, wkt: string, className: string = 'ol-layer') {
+  public createImageLayer(url: string, wkt: string, className: string = 'ol-layer', layer_id: string = '') {
     const feature = this.wktService.wktToFeature(wkt, 'EPSG:3857');
     const polygon = this.getPolygonFromFeature(feature, wkt);
 
@@ -62,34 +64,43 @@ export class BrowseOverlayService {
       sources: [new Static({
         url,
         imageExtent: polygon.getExtent(),
-        crossOrigin: '*.asf.alaska.edu' //hyp3-event-monitoring-productbucket-1t80jdtrfscje.s3.amazonaws.com'
+        // crossOrigin: '*'
+        crossOrigin: '*.asf.alaska.edu, *.hyp3-event-monitoring-productbucket-1t80jdtrfscje.s3.amazonaws.com'
       })],
-      operationType: 'pixel' as RasterOperationType,
-      operation: (p0: number[][], _) => {
-              var pixel = p0[0];
+      // operationType: 'pixel' as RasterOperationType,
+      // operation: (p0: number[][], _) => {
+      //         var pixel = p0[0];
 
-            var r = pixel[0];
-            var g = pixel[1];
-            var b = pixel[2];
+      //       var r = pixel[0];
+      //       var g = pixel[1];
+      //       var b = pixel[2];
 
-            if(r + g + b <= 10) {
-              return [0, 0, 0, 0];
-            }
-            // // CIE luminance for the RGB
-            // var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+      //       if(r + g + b <= 10) {
+      //         return [0, 0, 0, 0];
+      //       }
+      //       // // CIE luminance for the RGB
+      //       // var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-            // pixel[0] = v; // Red
-            // pixel[1] = v; // Green
-            // pixel[2] = v; // Blue
-            // //pixel[3] = 255;
+      //       // pixel[0] = v; // Red
+      //       // pixel[1] = v; // Green
+      //       // pixel[2] = v; // Blue
+      //       // //pixel[3] = 255;
 
-            return pixel;
-          }
+      //       return pixel;
+      //     }
       // opacity: this.browseLayer?.getOpacity() ?? 1.0
     });
 
-    const Imagelayer = new ImageLayer({source: rLayer, className});
+    const Imagelayer = new ImageLayer({
+      source: rLayer,
+      zIndex: 0,
+      extent: polygon.getExtent(),
+      opacity: 1.0,
+      className});
 
+    if(layer_id !== '') {
+      Imagelayer.set('layer_id', layer_id);
+    }
 
     return Imagelayer;
   }
