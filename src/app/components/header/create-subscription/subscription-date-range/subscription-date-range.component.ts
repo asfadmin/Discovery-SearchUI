1ef6c674b3289ee20923f2ca4194fd3d0f5b5221
codefactor-io <support@codefactor.io>
2021-12-11 01:56:15 +0000

    [CodeFactor] Apply fixes

--- src/app/components/header/create-subscription/subscription-date-range/subscription-date-range.component.ts
+++ src/app/components/header/create-subscription/subscription-date-range/subscription-date-range.component.ts
@@ -36,17 +36,17 @@ export class SubscriptionDateRangeComponent implements OnInit {
 
   public onStartDateError(): void {
     const min = new Date(this.minDate);
-    const dayBefore = moment(min.setDate(min.getDate() - 1)).format("MMMM Do YYYY");
+    const dayBefore = moment(min.setDate(min.getDate() - 1)).format('MMMM Do YYYY');
 
     this.notificationService.error(
       `subscription start date must be after ${dayBefore}`,
-      "Invalid Start Date");
+      'Invalid Start Date');
   }
 
   public onEndDateError(): void {
     this.notificationService.error(
-      "subscription end date must be within 6 months of current date",
-      "Invalid End Date");
+      'subscription end date must be within 6 months of current date',
+      'Invalid End Date');
   }
 
   private addDays(date: Date, numDays: number): Date {
