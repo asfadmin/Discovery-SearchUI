58b9e96163b3d22da60129c56a822b3b3b08deba
tylercchase <tylercchase@gmail.com>
2021-12-21 19:53:20 -0500

    Check user authentication before downloading

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -4,19 +4,44 @@ import { download, Download } from './download';
 import { Observable } from 'rxjs';
 import { SAVER, Saver } from '@services/saver.provider';
 
+import { Store } from '@ngrx/store';
+import { AppState } from '@store';
+
+import * as userStore from '@store/user';
+import { SubSink } from 'subsink';
+import { AuthService } from './auth.service';
+
 @Injectable({providedIn: 'root'})
 export class DownloadService {
 
+  public isUserLoggedIn: boolean;
+  private subs = new SubSink();
 
   constructor(
     private http: HttpClient,
-    @Inject(SAVER) private save: Saver
-  ) {}
+    @Inject(SAVER) private save: Saver,
+    private store$: Store<AppState>,
+    private Auth: AuthService
+    ) {
+      this.subs.add(
+        this.store$.select(userStore.getIsUserLoggedIn).subscribe(
+          isLoggedIn => this.isUserLoggedIn = isLoggedIn
+        )
+      );
+    }
 
   classicResp: Observable<Download>;
 
   download(url: string, filename: string): Observable<Download> {
-
+    if (!this.isUserLoggedIn) {
+      this.Auth.login$().toPromise().then(() => {
+        return this.downloadInternal(url, filename);
+      });
+    } else {
+      return this.downloadInternal(url, filename);
+    }
+  }
+  private downloadInternal(url: string, filename: string): Observable<Download> {
     const resp = this.http.get(url, {
       withCredentials: !(new URL(url).origin.startsWith('hyp3')),
       reportProgress: true,
