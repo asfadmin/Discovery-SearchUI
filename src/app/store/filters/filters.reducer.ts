69e64df0f85b941375f5cb0088c523d1dbf17d0d
codefactor-io <support@codefactor.io>
2022-04-05 22:49:52 +0000

    [CodeFactor] Apply fixes to commit 46de9a3

--- src/app/store/filters/filters.reducer.ts
+++ src/app/store/filters/filters.reducer.ts
@@ -728,7 +728,7 @@ export function filtersReducer(state = initState, action: FiltersActions): Filte
       return {
         ...state,
         hyp3ProductTypes: []
-      }
+      };
     }
     default: {
       return state;
