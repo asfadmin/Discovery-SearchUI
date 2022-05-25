b0e7ff66c89bc2ceec3a9e0f3003451b69075c2c
tylercchase <tylercchase@gmail.com>
2022-05-25 11:01:38 -0800

    basic baseline chart in d3

--- src/app/components/baseline-chart/baseline-chart.component.ts
+++ src/app/components/baseline-chart/baseline-chart.component.ts
@@ -3,16 +3,15 @@ import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/co
 import { combineLatest } from 'rxjs';
 import { map, tap, filter, } from 'rxjs/operators';
 
-import Chart from 'chart.js';
 import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import * as scenesStore from '@store/scenes';
 import * as queueStore from '@store/queue';
 
 import { SubSink } from 'subsink';
-import { ChartService, ScenesService } from '@services';
+import {  ScenesService } from '@services';
 import { criticalBaselineFor, CMRProduct } from '@models';
-
+import * as d3 from 'd3';
 export enum ChartDatasets {
   MASTER = 0,
   SELECTED = 1,
@@ -31,22 +30,30 @@ export enum ChartDatasets {
 export class BaselineChartComponent implements OnInit, OnDestroy {
   @ViewChild('baselineChart', { static: true }) baselineChart: ElementRef;
 
-  private chart: Chart;
   private criticalBaseline: number;
-  private hoveredProductId;
   private isFirstLoad = true;
   private offsets = { temporal: 0, perpendicular: 0 };
   private subs = new SubSink();
 
+  private data: {x: number, y: number, id?: string}[][] = [[], [], [], [], [], [], [], []];
+  private svg;
+
+  private margin = { top: 10, right: 30, bottom: 30, left: 60 };
+  private width = 840 - this.margin.left - this.margin.right;
+  private height = 350 - this.margin.top - this.margin.bottom;
+  private dotsContainer;
+
+  private x;
+  private y;
   constructor(
     private store$: Store<AppState>,
-    private chartService: ChartService,
     private scenesService: ScenesService,
   ) { }
 
   ngOnInit(): void {
-    this.initChart();
-
+    // this.initChart();
+    this.createSVG();
+    this.drawChart();
     const products$ = this.scenesService.scenes$().pipe(
       tap(products => products.map(
         product => this.criticalBaseline = criticalBaselineFor(product)
@@ -83,7 +90,7 @@ export class BaselineChartComponent implements OnInit, OnDestroy {
             this.isFirstLoad = false;
           }
 
-          this.chart.update();
+          // this.chart.update();
         })
     );
 
@@ -101,7 +108,7 @@ export class BaselineChartComponent implements OnInit, OnDestroy {
       ).subscribe(
         points => {
           this.setDataset(ChartDatasets.DOWNLOADS, points);
-          this.chart.update();
+          // this.chart.update();
         }
       )
     );
@@ -119,13 +126,68 @@ export class BaselineChartComponent implements OnInit, OnDestroy {
         map(this.productToPoint)
       ).subscribe(point => {
         this.setDataset(ChartDatasets.MASTER, [point]);
-        this.chart.update();
+        // this.chart.update();
       })
     );
   }
+  // private handleZoom(_event) {
+  //   console.log('ahhhhhhhhhhhhhh')
+  // }
+  private createSVG() {
+    this.svg = d3.select('div#baseline').append('svg')
+    .attr('width', this.width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.top  + this.margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
 
+  }
+  private drawChart() {
+
+    this.x = d3.scaleLinear()
+    .domain([-500, 3000])
+    .range([0, this.width]);
+  this.svg.append('g')
+    .attr('transform', `translate(0, ${this.height})`)
+    .call(d3.axisBottom(this.x));
+    this.y = d3.scaleLinear().domain([-200, 200]).range([this.height, 0]);
+    this.svg.append('g').call(d3.axisLeft(this.y));
+
+    // add dots of products
+    this.dotsContainer = this.svg.append('g')
+    .selectAll('circle')
+    .data(this.data[ChartDatasets.PRODUCTS])
+    .join()
+    .append('circle')
+    .attr('cx', d => this.x(d.x))
+    .attr('cy', d => this.y(d.y))
+    .attr('r', 5)
+    .attr('fill', '#00bcd4');
+    function handleStuff(e){
+      console.log(e);
+      console.log('ahhh');
+    }
+    const zoom = d3.zoom().on('zoom', handleStuff);
+    d3.select('svg').call(zoom);
+  }
   private setDataset(dataset: ChartDatasets, data) {
-    this.chart.data.datasets[dataset].data = data;
+    this.data[dataset] = data;
+    console.log(this.data);
+    const self = this;
+    this.dotsContainer.data(this.data[ChartDatasets.PRODUCTS]).join('circle')
+    .attr('cx', d => this.x(d.x))
+    .attr('cy', d => this.y(d.y))
+    .attr('r', 5)
+    .attr('fill', function (d) {
+      if (self.data[ChartDatasets.MASTER].length > 0 && self.data[ChartDatasets.MASTER][0]?.id === d.id) {
+        return 'black';
+      } else if (self.data[ChartDatasets.SELECTED].length > 0 && self.data[ChartDatasets.SELECTED][0]?.id === d.id) {
+        return 'red';
+      } else if (self.data[ChartDatasets.DOWNLOADS].some(p => p.id === d.id)) {
+        return 'blue';
+      } else {
+        return '#9e9e9e';
+      }
+    });
   }
 
   private productToPoint = (product: CMRProduct) => {
@@ -175,36 +237,38 @@ export class BaselineChartComponent implements OnInit, OnDestroy {
     return { min, max };
   }
 
-  private updateScales(extrema) {
-    const { min, max } = extrema;
-    const xBuffer = (max.x - min.x) * .25;
-    const yBuffer = (max.y - min.y) * .25;
+  private updateScales(_extrema) {
+    // const { min, max } = extrema;
+    // const xBuffer = (max.x - min.x) * .25;
+    // const yBuffer = (max.y - min.y) * .25;
 
-    this.chart.options.scales.xAxes[0].ticks.min = Math.floor((min.x - xBuffer) / 100) * 100;
-    this.chart.options.scales.xAxes[0].ticks.max = Math.ceil((max.x + xBuffer) / 100) * 100;
+    // TODO: update the graph scales things
 
-    this.chart.options.scales.yAxes[0].ticks.min = Math.floor((min.y - yBuffer) / 100) * 100;
-    this.chart.options.scales.yAxes[0].ticks.max = Math.ceil((max.y + yBuffer) / 100) * 100;
+    // this.chart.options.scales.xAxes[0].ticks.min = Math.floor((min.x - xBuffer) / 100) * 100;
+    // this.chart.options.scales.xAxes[0].ticks.max = Math.ceil((max.x + xBuffer) / 100) * 100;
+
+    // this.chart.options.scales.yAxes[0].ticks.min = Math.floor((min.y - yBuffer) / 100) * 100;
+    // this.chart.options.scales.yAxes[0].ticks.max = Math.ceil((max.y + yBuffer) / 100) * 100;
   }
 
-  private initChart() {
-    this.chart = this.chartService.makeChart(
-      this.baselineChart.nativeElement,
-      this.setHoveredItem,
-      this.onSelectHoveredScene
-    );
-  }
+  // private initChart() {
+  //   this.chart = this.chartService.makeChart(
+  //     this.baselineChart.nativeElement,
+  //     this.setHoveredItem,
+  //     this.onSelectHoveredScene
+  //   );
+  // }
 
-  private setHoveredItem = (tooltip, data) => {
-    const dataset = data.datasets[tooltip.datasetIndex].data;
+  // private setHoveredItem = (tooltip, data) => {
+  //   const dataset = data.datasets[tooltip.datasetIndex].data;
 
-    this.hoveredProductId = dataset[tooltip.index].id;
-  }
+  //   this.hoveredProductId = dataset[tooltip.index].id;
+  // }
 
-  private onSelectHoveredScene = () => {
-    const action = new scenesStore.SetSelectedScene(this.hoveredProductId);
-    this.store$.dispatch(action);
-  }
+  // private onSelectHoveredScene = () => {
+  //   const action = new scenesStore.SetSelectedScene(this.hoveredProductId);
+  //   this.store$.dispatch(action);
+  // }
 
   ngOnDestroy() {
     this.subs.unsubscribe();
