9f2f6c5c6249024ce8d0cdebde288557b9a18117
tcchase <tylercchase@gmail.com>
2022-11-01 10:37:53 -0400

    fix: default to light mode on no theme

--- src/app/services/url-state.service.ts
+++ src/app/services/url-state.service.ts
@@ -151,7 +151,7 @@ export class UrlStateService {
       const body = document.getElementsByTagName('body')[0];
       body.removeAttribute('class');
       body.classList.add(`theme-${profile.theme}`);
-    } else {
+    } else if(profile.theme){
       this.themeService.theme.pipe(take(1)).subscribe(
         themePreference => {
           const body = document.getElementsByTagName('body')[0];
@@ -159,6 +159,10 @@ export class UrlStateService {
           body.classList.add(`theme-${themePreference}`);
         }
       )
+    } else {
+      const body = document.getElementsByTagName('body')[0];
+      body.removeAttribute('class');
+      body.classList.add('theme-light');
     }
     const action = profile.mapLayer === models.MapLayerTypes.STREET ?
       new mapStore.SetStreetView() :
