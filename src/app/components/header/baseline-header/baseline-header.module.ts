f1244d230f9b5b7cb36856a2e96344eab52f8336
Yoreley <yoreley@gmail.com>
2023-03-13 14:58:18 -0700

    WIP Translate 7.0

--- src/app/components/header/baseline-header/baseline-header.module.ts
+++ src/app/components/header/baseline-header/baseline-header.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import {BaselineHeaderComponent} from "./baseline-header.component";
 
-
 @NgModule({
   declarations: [
     BaselineHeaderComponent,
   ],
   imports: [
     CommonModule,
+
 
   ],
   exports: [
