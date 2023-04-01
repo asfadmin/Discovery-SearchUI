d61def04651444d928706664aae3f52fa3e16de2
kim <jdfairbanks@alaska.edu>
2023-03-31 16:50:31 -0800

    processing level selector grayed out when bursts toggled

--- src/app/components/shared/selectors/burst-selector/burst-selector.component.ts
+++ src/app/components/shared/selectors/burst-selector/burst-selector.component.ts
@@ -4,6 +4,8 @@ import { sentinel_1 } from '@models/datasets';
 import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import * as filtersStore from '@store/filters';
+import { first, map } from 'rxjs';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-burst-selector',
@@ -12,10 +14,18 @@ import * as filtersStore from '@store/filters';
 })
 export class BurstSelectorComponent implements OnInit {
   @Output() toggled: EventEmitter<boolean> = new EventEmitter<boolean>(false)
+  private subs: SubSink = new SubSink()
+  constructor(private store$: Store<AppState>) {
 
-  constructor(private store$: Store<AppState>) { }
+  }
 
   ngOnInit(): void {
+    this.subs.add(
+      this.store$.select(filtersStore.getProductTypes).pipe(
+        first(),
+        map(datasetTypes => datasetTypes.map(p => p.apiValue).includes('BURST'))
+      ).subscribe( on => this.toggled.emit(on))
+    )
   }
 
   public onToggleBurst(on: MatSlideToggleChange): void {
--- src/app/components/shared/selectors/product-type-selector/product-type-selector.component.ts
+++ src/app/components/shared/selectors/product-type-selector/product-type-selector.component.ts
@@ -28,7 +28,7 @@ export class ProductTypeSelectorComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.subs.add(
-      this.datasetProductTypes$.subscribe(types => this.productTypesList = types.map(type => type.apiValue))
+      this.datasetProductTypes$.subscribe(types => this.productTypesList = types.map(type => type.apiValue).filter(v => v !== 'BURST'))
     );
   }
 
