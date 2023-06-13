69cc0805202594c1a72179fb749d1c64c70f62f5
Andrew Anderson <raanderson9@alaska.edu>
2023-06-13 13:00:40 -0700

    DS-4777 updated contact url

--- src/app/models/asf-website.model.ts
+++ src/app/models/asf-website.model.ts
@@ -2,7 +2,7 @@ export const baseUrl = 'https://www.asf.alaska.edu';
 
 export const asfWebsite = {
   home: baseUrl,
-  contact: `${baseUrl}/contact`,
+  contact: `${baseUrl}/asf/contact-us`,
   restricted_data_access: `${baseUrl}/restricted-data-access-request/`,
   api_get_data: `${baseUrl}/get-data/api/`,
 };
