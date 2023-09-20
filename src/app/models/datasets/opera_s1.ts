7ded8d505e647de3b00013c3d7a5865dcdf88757
codefactor-io <support@codefactor.io>
2023-09-20 18:13:50 +0000

    [CodeFactor] Apply fixes

--- src/app/models/datasets/opera_s1.ts
+++ src/app/models/datasets/opera_s1.ts
@@ -14,9 +14,9 @@ export const opera_s1 = {
     Props.BASELINE_TOOL,
     Props.SUBTYPE,
   ],
-  apiValue: { 
+  apiValue: {
     platform: 'SENTINEL-1',
-    collections: 
+    collections:
     "OPERA_L2_CSLC-S1-STATIC_PROVISIONAL_V0,OPERA_L2_CSLC-S1_PROVISIONAL_V0,OPERA_L2_RTC-S1-STATIC_PROVISIONAL_V0,OPERA_L2_RTC-S1_PROVISIONAL_V0,OPERA_L2_RTC-S1_V1,OPERA_L2_RTC-S1-STATIC_V1,OPERA_L2_CSLC-S1_V1,OPERA_L2_CSLC-S1-STATIC_V1" },
   date: { start: new Date('2014/06/15 03:44:43 UTC') },
   infoUrl: 'https://sentinels.copernicus.eu/web/sentinel/user-guides/sentinel-1-sar/acquisition-modes/interferometric-wide-swath',
