e9ed9c862c868eeeb07e7db2e9c7e797df0b9ddb
Andrew Anderson <raanderson9@alaska.edu>
2024-07-25 16:25:00 -0700

    WIP - chart resizing doesn't work

--- src/app/components/timeseries-chart/timeseries-chart.module.ts
+++ src/app/components/timeseries-chart/timeseries-chart.module.ts
@@ -7,17 +7,19 @@ import { TranslateModule } from "@ngx-translate/core";
 import { TimeseriesChartComponent } from './timeseries-chart.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import {ResizedEventModule} from '@directives/resized.directive';
 
 
 @NgModule({
   declarations: [TimeseriesChartComponent],
-  imports: [
-    CommonModule,
-    MatSharedModule,
-    TranslateModule,
-	MatTableModule,
-	MatIconModule
-  ],
+    imports: [
+        CommonModule,
+        MatSharedModule,
+        TranslateModule,
+        MatTableModule,
+        MatIconModule,
+        ResizedEventModule
+    ],
   exports: [
     TimeseriesChartComponent,
   ]
