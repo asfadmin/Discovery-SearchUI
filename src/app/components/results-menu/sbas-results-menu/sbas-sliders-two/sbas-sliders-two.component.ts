1ec0d81c7c591fad10f9f5efed788c2aa79f90e1
Andrew Anderson <raanderson9@alaska.edu>
2024-09-16 13:34:44 -0700

    Bug Fix
    Switching between SBAS and Baseline works. It didn't before. The chart filters would be FUBAR'd on Baseline.

--- src/app/components/results-menu/sbas-results-menu/sbas-sliders-two/sbas-sliders-two.component.ts
+++ src/app/components/results-menu/sbas-results-menu/sbas-sliders-two/sbas-sliders-two.component.ts
@@ -42,7 +42,7 @@ export class SbasSlidersTwoComponent implements OnInit {
   public metersValues$ = new Subject<number[]>();
   // public perpStart = 800;
 
-  // private firstLoad = true;
+  private firstLoad = true;
   private firstMeterLoad = true;
   private subs = new SubSink();
 
@@ -105,7 +105,9 @@ export class SbasSlidersTwoComponent implements OnInit {
     this.subs.add(
       this.store$.select(filtersStore.getTemporalRange).subscribe(
         temp => {
-          if (this.daysRange.start !== temp.start || this.daysRange.end !== temp.end) {
+          // if (this.daysRange.start !== temp.start || this.daysRange.end !== temp.end) {
+          if (this.firstLoad) {
+            this.firstLoad = false;
             this.daysRange = {start: temp.start, end: temp.end};
             this.slider.set([temp.start, temp.end]);
           }
