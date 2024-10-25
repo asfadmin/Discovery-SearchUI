a9a42eddec0cd5e9dee4691a7e0c20d3f760d35a
Yoreley <yvillafanez@unicon-intl.com>
2024-10-25 11:48:34 -0700

    Auto stash before merge of "yoreley/slider" and "asfadmin/displacement"

--- src/app/components/timeseries-chart/timeseries-chart-temporal-slider/timeseries-chart-temporal-slider.component.ts
+++ src/app/components/timeseries-chart/timeseries-chart-temporal-slider/timeseries-chart-temporal-slider.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import * as noUiSlider from 'nouislider';
 
 @Component({
   selector: 'app-timeseries-chart-temporal-slider',
   standalone: true,
-  imports: [],
   templateUrl: './timeseries-chart-temporal-slider.component.html',
-  styleUrl: './timeseries-chart-temporal-slider.component.scss'
+  styleUrls: ['./timeseries-chart-temporal-slider.component.scss']
 })
-export class TimeseriesChartTemporalSliderComponent {
+export class TimeseriesChartTemporalSliderComponent implements OnInit {
+  @ViewChild('slider', { static: true }) sliderRef: ElementRef;
 
+  constructor() { }
+
+  ngOnInit() {
+    this.makeSlider(this.sliderRef);
+  }
+
+  public makeSlider(filterRef: ElementRef) {
+    noUiSlider.create(filterRef.nativeElement, {
+      start: [2000, 2023], // Rango inicial con fechas
+      connect: true,
+      step: 1,
+      tooltips: {
+        to: function (value) {
+          return Math.round(value).toString(); // Muestra solo el año
+        }
+      },
+      range: {
+        'min': 2000,
+        'max': 2023
+      },
+      behaviour: 'tap-drag', // Mejora el comportamiento de arrastre
+      orientation: 'horizontal'
+    });
+  }
 }
