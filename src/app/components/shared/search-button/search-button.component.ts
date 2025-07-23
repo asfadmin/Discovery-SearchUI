0337188a772c21f4d034e5e7a936a6ed7d8a87db
Andrew Anderson <raanderson9@alaska.edu>
2025-07-23 15:30:34 -0700

    Tweaking

--- src/app/components/shared/search-button/search-button.component.ts
+++ src/app/components/shared/search-button/search-button.component.ts
@@ -347,7 +347,7 @@ export class SearchButtonComponent implements OnInit, OnDestroy {
 
   public exportAPI(): void {
     this.dialog.open(ApiLinkDialogComponent, {
-      minWidth: '425px',
+      minWidth: '375',
     });
   }
 
