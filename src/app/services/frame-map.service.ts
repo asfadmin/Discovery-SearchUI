7a2ebc8c3db25c6e0ef7d0f957d3904fed5bf840
Andrew Anderson <raanderson9@alaska.edu>
2025-08-19 10:44:29 -0700

    DS-6142 WIP
    Download service for frame maps in-place. Need to make a place(s) on the UI to make it happen.

--- src/app/services/frame-map.service.ts
+++ src/app/services/frame-map.service.ts
@@ -13,37 +13,15 @@ export class FrameMapService {
     this.datasets.forEach((dataset) => {dataset.id === id ? this.downloadFileFromUrl(dataset.frameMap.ascending, dataset.name) : null;});
   }
 
-  //  /**
-  //   * Opens the user's system file dialog prompting to download
-  //   * the given data.
-  //   *
-  //   * @param fileName default name of the saved file. This is what will show up as file name in the user's file dialog.
-  //   * @param data the content of the file.
-  //   * @param mime
-  //   * @param bom
-  //   */
-  // private download(
-  //   fileName: string,
-  //   data: string | ArrayBuffer | ArrayBufferView | Blob,
-  //   mime = 'application/json',
-  //   bom?: string | Uint8Array,
-  // ) {
-  //   const blobData = bom === undefined ? [data] : [bom, data]
-  //   const blob = new Blob(blobData, { type: mime })
-  //   const a = document.createElement("a")
-  //
-  //   a.download = fileName
-  //   a.href = URL.createObjectURL(blob)
-  //   a.click()
-  //   setTimeout(() => {
-  //     URL.revokeObjectURL(a.href)
-  //     a.remove()
-  //   }, 200)
-  // }
-
+  /**
+   * Downloads a file from a given URL and saves it with the specified file name.
+   * @param url - The URL of the file to download.
+   * @param fileName - The name to save the downloaded file as.
+   *
+   * NOTE: This will fail when run locally with a cross-origin request error.
+   * It is intended to be used in a production environment where the URL is accessible.
+   */
   async downloadFileFromUrl(url: string, fileName: string): Promise<void> {
-    console.log('Downloading from URL:', url);
-    console.log('File name:', fileName);
     try {
       const response = await fetch(url);
       if (!response.ok) {
