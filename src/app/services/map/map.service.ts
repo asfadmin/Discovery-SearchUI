6db8184c0e57fa5542222307db174c2e468a5017
tylercchase <tylercchase@gmail.com>
2025-10-01 15:28:25 -0400

    feat: aria frame map filters and sbas

--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -387,7 +387,7 @@ export class MapService implements OnDestroy {
     this.map = this.updatedMap();
   }
 
-  public setFrameSelectionActive(active: boolean, url?: string, frameRange?: models.Range<number | null>) {
+  public setFrameSelectionActive(active: boolean, url?: string, frameRange?: models.Range<number | null>, pathRange?: models.Range<number | null>) {
     if (!active) {
       this.frameSelectionOverlay.setVisible(false);
       this.selectClick?.getFeatures().clear();
@@ -427,18 +427,32 @@ export class MapService implements OnDestroy {
 
     source.on('featuresloadend', () => {
       if (this.frameSelectionOverlay.getSourceState() === 'ready') {
-        this.filterFrameOverlay(frameRange);
+        this.filterFrameOverlay(frameRange, pathRange);
       }
     })
   }
-  public filterFrameOverlay(frame: models.Range<number | null>) {
-    if (frame.start === null) {
-      return;
+
+  public isWithinRange(value, range: models.Range<number | null>) {
+    if(range === null || range == undefined) {
+      return true;
     }
-    this.frameSelectionOverlay?.getSource()?.getFeatures().forEach(a => {
-      if (+a.get('path') !== frame.start) {
-        a.setStyle(new Style({}));
+    if(range?.start !== null ) {
+      if(range?.end !== null) {
+        return value >= range?.start && value <= range?.end;
       } else {
+        return value >= range?.start;
+      }
+    } else if(range?.end !== null) {
+      return value <= range.end;
+    }
+    return true;
+  }
+  public filterFrameOverlay(frameRange: models.Range<number | null>, pathRange: models.Range<number | null>) {
+    this.frameSelectionOverlay?.getSource()?.getFeatures().forEach(a => {
+      let frame = +a.get('id');
+      let path = +a.get('path');
+
+      if (this.isWithinRange(frame, frameRange) && this.isWithinRange(path, pathRange)) {
         a.setStyle(new Style({
           fill: new Fill({
             color: '#FFFFFF33',
@@ -447,6 +461,8 @@ export class MapService implements OnDestroy {
             color: 'black',
           })
         }));
+      } else {
+        a.setStyle(new Style({}));
       }
     })
 
@@ -611,9 +627,14 @@ export class MapService implements OnDestroy {
       // this.map.addLayer(this.selectedOnDemandFrameOverlays)
     }
   }
-  public sbasFrameMode(enabled) {
+  public sbasFrameMode(enabled: boolean, frame: string, dataset: models.Dataset) {
     this.polygonLayer.setVisible(enabled);
     this.selectedLayer.setVisible(enabled);
+    if(!enabled) {
+      this.setFrameSelectionActive(true, dataset.frameMap.ascending, {start: +frame, end: +frame});
+    } else {
+      this.setFrameSelectionActive(false);
+    }
   }
 
   private setMap(mapView: views.MapView, overlay): void {
