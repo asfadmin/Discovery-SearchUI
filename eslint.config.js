fa49e006498ef601f53f37467269b2a72567765d
tylercchase <tylercchase@gmail.com>
2025-10-14 14:42:00 -0400

    style: lint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
 const prettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
@@ -15,35 +15,35 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
       ],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
+      '@typescript-eslint/no-unused-vars': [
+        'error',
         {
-          "varsIgnorePattern": "^_",
-          "argsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_"
-        }
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
       ],
       // ---------
       // re-enable these at some point
       // ---------
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-prototype-builtins": "off",
-      "@typescript-eslint/no-this-alias": "off",
-      "no-useless-escape": "off",
-      "@angular-eslint/no-output-on-prefix": "off",
-      "@angular-eslint/no-output-native": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-prototype-builtins': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      'no-useless-escape': 'off',
+      '@angular-eslint/no-output-on-prefix': 'off',
+      '@angular-eslint/no-output-native': 'off',
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
@@ -52,28 +52,25 @@ module.exports = tseslint.config(
       // ---------
       // re-enable these at some point
       // ---------
-      "@angular-eslint/template/click-events-have-key-events": "off",
-      "@angular-eslint/template/interactive-supports-focus": "off",
-      "@angular-eslint/template/alt-text": "off",
-      "@angular-eslint/template/no-negated-async": "off",
-      "@angular-eslint/template/mouse-events-have-key-events": "off",
-      "@angular-eslint/template/label-has-associated-control": "off",
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+      '@angular-eslint/template/alt-text': 'off',
+      '@angular-eslint/template/no-negated-async': 'off',
+      '@angular-eslint/template/mouse-events-have-key-events': 'off',
+      '@angular-eslint/template/label-has-associated-control': 'off',
     },
   },
   {
-    files: ["**/*"],
-    extends: [
-      prettierRecommended,
-    ],
+    files: ['**/*'],
+    extends: [prettierRecommended],
     rules: {
-      "prettier/prettier": [
-        "error",
+      'prettier/prettier': [
+        'error',
         {
-          "singleQuote": true
+          singleQuote: true,
         },
-        {
-        }
+        {},
       ],
-    }
-  }
+    },
+  },
 );
