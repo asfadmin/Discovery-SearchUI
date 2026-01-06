b10094deb215688f4721c4937560af3e7a15c6ef
tcchase <tylercchase@gmail.com>
2026-01-06 15:59:12 -0500

    fix: regular test using wrong url for tea

--- src/app/services/envs/env-test.ts
+++ src/app/services/envs/env-test.ts
@@ -12,7 +12,7 @@ export const env = {
   test: {
     api: 'https://api-test.asf.alaska.edu',
     api_maturity: 'prod',
-    auth: 'https://sentinel1-test.asf.alaska.edu',
+    auth: 'https://cumulus.asf.alaska.edu',
     urs: 'https://urs.earthdata.nasa.gov',
     urs_client_id: 'BO_n7nTIlMljdvU6kRRB3g',
     banner: 'https://banners.asf.alaska.edu',
