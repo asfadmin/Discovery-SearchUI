c944628c33d9a08938743207064f8f78bb19f8e5
Tyler Chase <tylercchase@gmail.com>
2026-02-24 14:23:35 -0500

    Revert "Add Dataset Selector search filter input"

--- src/app/components/shared/selectors/dataset-selector/dataset-selector.component.ts
+++ src/app/components/shared/selectors/dataset-selector/dataset-selector.component.ts
@@ -1,27 +1,21 @@
 import {
   Component,
+  Input,
   Output,
   EventEmitter,
   inject,
   input,
   computed,
-  Signal,
-  signal,
-  viewChild,
-  ElementRef,
 } from '@angular/core';
 
 import * as models from '@models';
 import { ScreenSizeService } from '@services';
 import { MatMenuTrigger, MatMenu, MatMenuItem } from '@angular/material/menu';
 import { AsyncPipe } from '@angular/common';
-import { MatLabel, MatInputModule } from '@angular/material/input';
-import { MatButton, MatIconButton } from '@angular/material/button';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatLabel } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { TranslateModule } from '@ngx-translate/core';
 import { DatasetComponent } from './dataset/dataset.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material/icon';
 
 // Declare GTM dataLayer array.
 declare global {
@@ -43,59 +37,29 @@ declare global {
     AsyncPipe,
     TranslateModule,
     DatasetComponent,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatIconModule,
-    MatIconButton,
   ],
 })
 export class DatasetSelectorComponent {
   private screenSize = inject(ScreenSizeService);
-  private translate = inject(TranslateService);
+
+  @Input() datasets: models.Dataset[];
+
   @Output() selectedChange = new EventEmitter<string>();
 
-  public datasets = input<models.Dataset[]>();
   public selected = input<string>();
-
-  public datasetFilter = signal<string>(null);
   public breakpoint$ = this.screenSize.breakpoint$;
   public breakpoints = models.Breakpoints;
   public isReadMore = true;
 
-  public searchElement = viewChild<ElementRef>('datasetOptions');
-
   public datasetName = computed(() => {
     let datasetName = '';
-    this.datasets().forEach((dataset) => {
+    this.datasets.forEach((dataset) => {
       if (dataset.id === this.selected()) {
         datasetName = dataset.name;
       }
     });
     return datasetName;
   });
-
-  public filteredDatasets: Signal<models.Dataset[]> = computed(() => {
-    if (!this.datasetFilter()) {
-      return this.datasets();
-    }
-    const userInput = this.datasetFilter().toUpperCase();
-    return this.datasets().filter((dataset) => {
-      return (
-        dataset.name.toUpperCase().includes(userInput) ||
-        this.translate
-          .instant(dataset.platformDesc)
-          .toUpperCase()
-          .includes(userInput) ||
-        dataset.source.name.includes(userInput)
-      );
-    });
-  });
-
-  public menuOpen() {
-    this.searchElement().nativeElement.focus();
-  }
-
   public onSelectionChange(dataset: string): void {
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
