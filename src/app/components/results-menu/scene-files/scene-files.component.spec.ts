df44ed5eb30f69e913ee73c7961383eca3d8c95d
Tyler Chase <tylercchase@gmail.com>
2026-03-09 14:49:31 -0400

    feat: vitest and playwright setup
    * chore: update test dependencies
    
    * chore: update testing to use tsconfig
    
    * chore: compiling test
    
    * chore: fix component testing
    
    Temp patch for this to get it working, will need some redesign to make something scalable for the future and other components that we are wanting to test.
    
    * chore: readd polyfill script to tsconfig
    
    * chore: add canvas package
    
    * chore: add code coverage support
    
    * chore: initial e2e test setup
    
    * chore: remove vitest browser mode test
    
    * fix: ngx-translate updates
    
    * Merge branch 'test' into tyler/vitest
    
    * feat: playwright github action
    
    * chore(deps): update package-lock
    
    * feat: authenticated testing framework
    
    * fix: auth action pathing
    
    * fix: action pathing
    
    * fix: directory pathing
    
    * fix: directory structure first
    
    * fix: use base64 creds for better parsing
    
    * feat: some example tests
    
    * feat: add ng test github action
    
    * chore: cleanup old screenshots
    
    * chore: remove test branch from action, set permissions explicitly
    
    * fix: explicit product scene list click
    
    * fix: specify timezoneId for tests
    
    * fix: bypassCSP for map based interaction setup
    
    * feat: aoi parsing test
    
    This shows an example of how to get info form a material input. The automated tool doesn't work for them out of the box.
    
    * chore: codefactor fix

--- src/app/components/results-menu/scene-files/scene-files.component.spec.ts
+++ src/app/components/results-menu/scene-files/scene-files.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SceneFilesComponent } from './scene-files.component';
-import { SceneFilesModule } from './scene-files.module';
-import { provideMockStore } from '@ngrx/store/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ToastrModule } from 'ngx-toastr';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initState as filtersInit } from '@store/filters/filters.reducer';
+import { initState as scenesInit } from '@store/scenes/scenes.reducer';
+import { initState as userInit } from '@store/user/user.reducer';
+import { initState as queueInit } from '@store/queue/queue.reducer';
+import { initState as hyp3Init } from '@store/hyp3/hyp3.reducer';
+import { initState as searchInit } from '@store/search/search.reducer';
+import { provideTranslateService } from '@ngx-translate/core';
 
 describe('SceneFilesComponent', () => {
   let component: SceneFilesComponent;
@@ -12,15 +19,26 @@ describe('SceneFilesComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        SceneFilesModule,
+        SceneFilesComponent,
         ToastrModule.forRoot({
           positionClass: 'toast-bottom-right',
         }),
       ],
       providers: [
-        provideMockStore(),
         provideHttpClient(),
         provideHttpClientTesting(),
+        provideTranslateService(),
+        // we'd need to do this for any component that needed store access.
+        provideMockStore({
+          initialState: {
+            filters: filtersInit,
+            scenes: scenesInit,
+            user: userInit,
+            queue: queueInit,
+            hyp3: hyp3Init,
+            search: searchInit,
+          },
+        }),
       ],
     });
   });
