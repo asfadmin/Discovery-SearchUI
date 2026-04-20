929836d15bcb90d1683a4b7b5bc876919cbfceda
tcchase <tylercchase@gmail.com>
2026-04-20 12:18:00 -0400

    chore: remove extra module addition

--- src/app/components/results-menu/scene-files/scene-file/scene-file.component.ts
+++ src/app/components/results-menu/scene-files/scene-file/scene-file.component.ts
@@ -85,7 +85,6 @@ import { fas, faSpinner } from '@fortawesome/free-solid-svg-icons';
     ReadableSizeFromBytesPipe,
     FullDatePipe,
     TranslateModule,
-    FontAwesomeModule,
   ],
 })
 export class SceneFileComponent implements OnInit, OnDestroy {
