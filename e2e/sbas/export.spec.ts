063875abf06ff77e82bcf0aa3730b55c6b99e1f8
tcchase <tylercchase@gmail.com>
2026-04-21 17:05:33 -0400

    chore: update search selector in sbas export test

--- e2e/sbas/export.spec.ts
+++ e2e/sbas/export.spec.ts
@@ -15,7 +15,7 @@ test('SBAS Download Pair CSV', async ({ page }) => {
       'S1B_WV_SLC__1SSV_20200720T132328_20200720T135106_022555_02ACF6_F823',
     );
   await page
-    .locator('#mat-button-toggle-6-button')
+    .getByText('Cancel SEARCH arrow_drop_down')
     .getByRole('button', { name: 'SEARCH' })
     .click();
   const downloadPromise = page.waitForEvent('download');
