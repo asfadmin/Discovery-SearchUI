77bdf6631347e13ddd967046c625301f823a6d0e
Princesa Yoreley <yoreley@gmail.com>
2026-04-23 12:50:03 -0700

    Test SBAS Start & End Date Filters

---
+++ e2e/sbas/date-filters.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+test('SBAS Start & End Date Filters', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Geographic Search' }).click();
+  await page
+    .getByRole('menuitem', { name: 'SBAS SBAS search provides' })
+    .click();
+  await page
+    .getByRole('region', { name: 'Scene' })
+    .getByLabel('Scene')
+    .fill('S1A_IW_SLC__1SDV_20180616T210817_20180616T210845_022387_026C91_EDAA');
+  await page
+    .locator('app-search-button')
+    .getByRole('button', { name: /^(SEARCH|UPDATE)$/ })
+    .click();
+
+  await page
+    .locator('mat-button-toggle')
+    .filter({ hasText: 'SBAS Filters' })
+    .click();
+
+  await page.getByRole('textbox', { name: 'Start Date' }).fill('9/1/2018');
+  await page.getByRole('textbox', { name: 'End Date' }).fill('11/1/2020');
+
+  await page
+    .locator('app-search-button')
+    .getByRole('button', { name: /^(SEARCH|UPDATE)$/ })
+    .click();
+
+  await expect(page.locator('app-scenes-list-header')).toContainText('67 Pairs');
+});
