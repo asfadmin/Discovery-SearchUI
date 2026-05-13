a11df2bce7ecc41432cf195c4379f5a899e5c5c4
tcchase <tylercchase@gmail.com>
2026-05-13 15:21:25 -0400

    feat: final saved search tests

--- e2e/profile/edit-search.spec.ts
+++ e2e/profile/edit-search.spec.ts
@@ -54,3 +54,47 @@ test(
     ).not.toBeVisible();
   },
 );
+
+test(
+  'Profile: Update saved search',
+  { tag: '@auth' },
+  async ({ loggedInPage }) => {
+    await loggedInPage.goto('/');
+
+    await loggedInPage
+      .getByRole('button', { name: 'Filters', exact: true })
+      .click();
+    await loggedInPage.locator('#mat-button-toggle-7').click();
+    await loggedInPage
+      .getByRole('menuitem', { name: 'Saved Searches' })
+      .click();
+    await loggedInPage.getByRole('menuitem', { name: 'Save Search' }).click();
+    await loggedInPage.getByRole('button', { name: 'Save Search' }).click();
+    await loggedInPage.getByRole('button', { name: 'Done' }).click();
+
+    await loggedInPage
+      .locator('div')
+      .filter({ hasText: /^File Type$/ })
+      .first()
+      .click();
+    await loggedInPage
+      .getByRole('option', { name: 'L1 Detected High-Res Dual-Pol' })
+      .click();
+    await loggedInPage.locator('.cdk-overlay-backdrop').click();
+
+    await loggedInPage
+      .getByRole('button', { name: 'automatedtesting_fullaccess' })
+      .click();
+    await loggedInPage
+      .getByRole('menuitem', { name: 'Saved Searches' })
+      .click();
+    await loggedInPage.getByText('keyboard_arrow_right').click();
+    await expect(loggedInPage.locator('app-saved-search')).not.toContainText(
+      'File Types: GRD_HD Update',
+    );
+    await loggedInPage.getByRole('button', { name: 'Update' }).click();
+    await expect(loggedInPage.locator('app-saved-search')).toContainText(
+      'Dataset: SENTINEL-1 File Types: GRD_HD Update',
+    );
+  },
+);
