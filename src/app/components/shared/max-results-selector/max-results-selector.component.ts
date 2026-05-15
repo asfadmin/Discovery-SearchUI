98c1b695e94d06f08025fb415d45206866781de5
William Horn <wbhorn@alaska.edu>
2026-05-15 10:22:32 -0800

    chore: refactor max-results-selector

--- src/app/components/shared/max-results-selector/max-results-selector.component.ts
+++ src/app/components/shared/max-results-selector/max-results-selector.component.ts
@@ -4,7 +4,6 @@ import { Store } from '@ngrx/store';
 import { AppState } from '@store';
 import * as searchStore from '@store/search';
 import * as filtersStore from '@store/filters';
-import * as scenesStore from '@store/scenes';
 
 import * as models from '@models';
 import { SubSink } from 'subsink';
@@ -19,6 +18,7 @@ import { DocsModalComponent } from '../docs-modal/docs-modal.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { fas, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { Pipe, PipeTransform } from '@angular/core';
+import { combineLatest } from 'rxjs';
 
 @Pipe({
   name: 'formatNumber',
@@ -27,11 +27,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatNumberPipe implements PipeTransform {
   transform(num: number | string): string {
     const n = typeof num === 'string' ? parseFloat(num) : num;
-
     if (isNaN(n)) {
       return '';
     }
-
     return n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   }
 }
@@ -58,74 +56,63 @@ export class MaxResultsSelectorComponent implements OnInit, OnDestroy {
   private sceneService = inject(ScenesService);
 
   public maxResults: number;
-  public numberOfScenes: number;
   public isMaxResultsLoading: boolean;
-  public currentSearchAmount: number;
   public areResultsLoaded = false;
 
   public searchType: models.SearchType;
   public searchTypes = models.SearchType;
-  public sbasProducts: models.CMRProduct[];
 
+  public isDataset = false;
+  public totalResultsCount = 0;
   public burstXMLFileCount = 0;
 
   public possibleMaxResults = [250, 500, 1000, 2000];
   private subs = new SubSink();
+
   constructor() {
     const library = inject(FaIconLibrary);
-
     library.addIconPacks(fas);
     library.addIcons(faSpinner);
   }
+
   ngOnInit() {
     this.subs.add(
       this.store$
-        .select(searchStore.getSearchType)
-        .subscribe((searchType) => (this.searchType = searchType)),
-    );
-
-    this.subs.add(
-      this.store$
         .select(filtersStore.getMaxSearchResults)
-        .subscribe((maxResults) => {
-          this.maxResults = maxResults;
-        }),
+        .subscribe((maxResults) => (this.maxResults = maxResults)),
     );
 
     this.subs.add(
-      this.store$
-        .select(searchStore.getIsMaxResultsLoading)
-        .subscribe((isLoading) => (this.isMaxResultsLoading = isLoading)),
-    );
+      combineLatest([
+        this.store$.select(searchStore.getSearchType),
+        this.store$.select(searchStore.getSearchAmount),
+        this.sceneService.scenes$,
+        this.pairService.productsFromPairs$,
+      ]).subscribe(([searchType, amount, scenes, sbasProducts]) => {
+        this.searchType = searchType;
+        this.isDataset = searchType === this.searchTypes.DATASET;
 
-    this.subs.add(
-      this.store$
-        .select(searchStore.getSearchAmount)
-        .subscribe(
-          (amount) =>
-            (this.currentSearchAmount = Number.isNaN(amount) ? 0 : amount),
-        ),
-    );
+        const currentSearchAmount = Number.isNaN(amount) ? 0 : amount;
 
-    this.subs.add(
-      this.store$
-        .select(scenesStore.getAreResultsLoaded)
-        .subscribe((areLoaded) => (this.areResultsLoaded = areLoaded)),
-    );
-
-    this.subs.add(
-      this.sceneService.scenes$.subscribe((scenes) => {
-        this.numberOfScenes = scenes.length;
         this.burstXMLFileCount = scenes.filter(
           (p) => p.metadata.productType === 'BURST',
         ).length;
-      }),
-    );
 
-    this.subs.add(
-      this.pairService.productsFromPairs$.subscribe(
-        (products) => (this.sbasProducts = products),
-      ),
+        switch (searchType) {
+          case this.searchTypes.SBAS:
+            this.totalResultsCount = sbasProducts?.length ?? 0;
+            break;
+          case this.searchTypes.CUSTOM_PRODUCTS:
+            this.totalResultsCount = scenes.length;
+            break;
+          case this.searchTypes.DATASET:
+            this.totalResultsCount =
+              currentSearchAmount + this.burstXMLFileCount;
+            break;
+          default:
+            this.totalResultsCount = currentSearchAmount;
+        }
+      }),
     );
   }
 
