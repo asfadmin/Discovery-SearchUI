234e96879be9fea718bdb41fc050f7ad62cc5f1c
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-05-15 15:57:55 -0800

    chore: drop outdated UAVSAR filters, product types limited to two now

--- src/app/models/datasets/uavsar.ts
+++ src/app/models/datasets/uavsar.ts
@@ -5,12 +5,7 @@ export const uavsar = {
   name: 'UAVSAR',
   subName: '',
   beta: false,
-  properties: [
-    Props.DATE,
-    Props.BEAM_MODE,
-    Props.POLARIZATION,
-    Props.MISSION_NAME,
-  ],
+  properties: [Props.DATE, Props.MISSION_NAME],
   apiValue: { dataset: 'UAVSAR' },
   date: { start: new Date('2008/04/28 21:10:16 UTC') },
   infoUrl: 'https://asf.alaska.edu/data-sets/sar-data-sets/uavsar/',
@@ -23,68 +18,16 @@ export const uavsar = {
   },
   productTypes: [
     {
-      apiValue: 'AMPLITUDE',
-      displayName: 'Amplitude',
+      apiValue: 'POLSAR',
+      displayName: 'Polarimetric Synthetic Aperture Radar (POLSAR)',
     },
     {
-      apiValue: 'STOKES',
-      displayName: 'Compressed Stokes Matrix',
-    },
-    {
-      apiValue: 'AMPLITUDE_GRD',
-      displayName: 'Ground Projected Amplitude',
-    },
-    {
-      apiValue: 'PROJECTED',
-      displayName: 'Ground Projected Complex',
-    },
-    {
-      apiValue: 'PROJECTED_ML3X3',
-      displayName: 'Ground Projected Complex, 3X3 Resampled',
-    },
-    {
-      apiValue: 'PROJECTED_ML5X5',
-      displayName: 'Ground Projected Complex, 5X5 Resampled',
-    },
-    {
-      apiValue: 'INTERFEROMETRY_GRD',
-      displayName: 'Ground Projected Interferogram',
-    },
-    {
-      apiValue: 'INTERFEROMETRY',
-      displayName: 'Interferogram',
-    },
-    {
-      apiValue: 'COMPLEX',
-      displayName: 'Multi-look Complex',
-    },
-    {
-      apiValue: 'KMZ',
-      displayName: 'GoogleEarth KMZ',
-    },
-    {
-      apiValue: 'INC',
-      displayName: 'Incidence angle',
-    },
-    {
-      apiValue: 'SLOPE',
-      displayName: 'Slope',
-    },
-    {
-      apiValue: 'DEM_TIFF',
-      displayName: 'GEOTIFF Height File',
-    },
-    {
-      apiValue: 'PAULI',
-      displayName: 'Pauli Decomposition',
-    },
-    {
-      apiValue: 'METADATA',
-      displayName: 'Annotation file / Metadata',
+      apiValue: 'RPI',
+      displayName: 'Repeat Pass Interferometry (RPI)',
     },
   ],
-  beamModes: ['POL', 'RPI'],
-  polarizations: ['Full', 'HH'],
+  beamModes: [],
+  polarizations: [],
   subtypes: [],
   platformDesc: 'UAVSAR_DESC',
   platformIcon: '/assets/icons/flight_black_48dp.svg',
