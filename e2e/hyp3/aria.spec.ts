340199630f7c14981c0aeff2049ba3e801ac9cf9
tcchase <tylercchase@gmail.com>
2026-05-19 17:57:42 -0400

    fix: use better mouse api

--- e2e/hyp3/aria.spec.ts
+++ e2e/hyp3/aria.spec.ts
@@ -14,16 +14,7 @@ test(
       .getByRole('menuitem', { name: 'ARIA S1 GUNW NISAR-format' })
       .click();
     await loggedInPage.getByRole('switch', { name: 'On Demand' }).click();
-
-    await loggedInPage
-      .locator('canvas')
-      .nth(1)
-      .click({
-        position: {
-          x: 790,
-          y: 347,
-        },
-      });
+    await loggedInPage.mouse.click(790, 503);
     await loggedInPage
       .getByRole('button', { name: 'Build SBAS SLC Stack' })
       .click();
