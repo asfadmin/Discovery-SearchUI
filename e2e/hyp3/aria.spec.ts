4f60db15f5dc77018783942d3ce0056e0e59c17d
Tyler Chase <tylercchase@gmail.com>
2026-05-20 15:07:05 -0400

    On Demand playwright tests (#2575)
    * feat: initial hyp3 tests
    
    * fix: update test job times
    
    * feat: zoom to results and mock data refinement
    
    * feat: job submission tests
    
    * fix: on demand other browser testing
    
    * fix: on demand zoom test
    
    * fix: try more specific selector for map scale for firefox (for ci)
    
    * fix: general zoom check
    
    * feat: use mock tile to help openlayers errors
    
    * feat: aria s1 gunw on demand test
    
    * feat: mock aria gunw frame map
    
    * fix: use better mouse api
    
    * fix: wait for aria map to load into openlayers
    
    * fix: try looping clicks
    
    * fix: don't run webgl dependent tests on firefox
    
    * chore: update aria label of on demand button

---
+++ e2e/hyp3/aria.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from 'e2e/pages/auth.page';
+test(
+  'On Demand: Aria On Demand',
+  { tag: ['@auth', '@webgl'] },
+  async ({ loggedInPage }) => {
+    await loggedInPage.goto('/');
+    await loggedInPage.route('**ARIA_S1_GUNW/**.geojson', async (request) => {
+      await request.fulfill({
+        path: 'e2e/hyp3/assets/aria_frame_map.geojson',
+      });
+    });
+    await loggedInPage.getByRole('button', { name: 'Sentinel-' }).click();
+    await loggedInPage
+      .getByRole('menuitem', { name: 'ARIA S1 GUNW NISAR-format' })
+      .click();
+    await loggedInPage.getByRole('switch', { name: 'On Demand' }).click();
+    await loggedInPage.waitForTimeout(1000);
+    await loggedInPage.mouse.click(790, 503);
+    await loggedInPage
+      .getByRole('button', { name: 'Build SBAS SLC Stack' })
+      .click();
+    await expect(
+      loggedInPage.locator('app-search-type-selector'),
+    ).toContainText('SBAS Search'); // just check that we made it to SBAS for now
+  },
+);
