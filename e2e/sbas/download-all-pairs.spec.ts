beb7ca0eda1d6cde48e9414be5cddbc77d568a6b
tcchase <tylercchase@gmail.com>
2026-05-28 14:00:29 -0400

    chore: codefactor feedback

--- e2e/sbas/download-all-pairs.spec.ts
+++ e2e/sbas/download-all-pairs.spec.ts
@@ -17,10 +17,6 @@ test('SBAS: Download All Pairs', async ({ page }) => {
     .getByRole('button', { name: 'Filters panel search button' })
     .click();
 
-  const sbasFiltersButton = page
-    .locator('mat-button-toggle')
-    .filter({ hasText: 'SBAS Filters' });
-
   const scenesListHeader = page.locator('app-scenes-list-header');
   await expect(scenesListHeader).toContainText(/\d+\s+Pairs?/i);
 
