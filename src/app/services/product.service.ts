6a34f98da5d07104babe0b125e266d1da7d10cbf
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-05-28 11:29:45 -0800

    style: clean up

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -601,7 +601,7 @@ export class ProductService {
       const polarizationMatch = /.*_([AB])_([VH]{4}|[VH]{2})\.(png|kml)/;
       const matched = p.match(polarizationMatch) ?? [];
       if (matched.length > 0) {
-        const [_, frequency, polarization] = [...matched];
+        const [frequency, polarization] = [matched[1], matched[2]];
         productTypeDisplay = `Frequency ${frequency} ${polarization} ${productTypeDisplay}`;
       }
 
