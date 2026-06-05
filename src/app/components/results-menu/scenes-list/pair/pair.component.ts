e2431791ab1ef86da7e234047636b2332ba0af9a
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2026-06-05 13:41:57 -0800

    bug: mvoe isS1DPair to html template directly

--- src/app/components/results-menu/scenes-list/pair/pair.component.ts
+++ src/app/components/results-menu/scenes-list/pair/pair.component.ts
@@ -54,17 +54,9 @@ export class PairComponent implements OnInit, OnDestroy {
   public hovered = false;
 
   public selectedPair: string[];
-  public isS1DPair = false;
   private subs = new SubSink();
 
   ngOnInit(): void {
-    if (
-      this.pair[0].dataset === 'SENTINEL-1D' ||
-      this.pair[1].dataset === 'SENTINEL-1D'
-    ) {
-      this.isS1DPair = this.pair[0].metadata.productType === 'BURST';
-    }
-
     this.subs.add(
       this.store$
         .select(scenesStore.getSelectedPairIds)
