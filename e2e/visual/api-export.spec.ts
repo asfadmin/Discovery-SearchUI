0fd2874ecb0f077de77f4f5c93faebad87d04663
tcchase <tylercchase@gmail.com>
2026-06-10 10:25:59 -0400

    fix: export visual tests

--- e2e/visual/api-export.spec.ts
+++ e2e/visual/api-export.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from 'e2e/fixtures';
 test('Ensure api export styling remains unchanged.', async ({ page }) => {
   await page.goto('/');
   await page.locator('#mat-button-toggle-9-button').click();
+  await page.getByRole('menuitem', { name: 'Export' }).click();
   await page.getByRole('menuitem', { name: 'Export API' }).click();
 
   await expect(page).toHaveScreenshot();
