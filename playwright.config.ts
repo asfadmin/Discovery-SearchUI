72198041717cf46f28142e735f2f48f42538b73d
tcchase <tylercchase@gmail.com>
2026-06-10 17:40:50 -0400

    chore: set tests to only do snapshot tests in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,6 +16,8 @@ export default defineConfig({
   // workers: process.env['CI'] ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env['CI'] ? 'blob' : 'html',
+  /* Only run snapshot tests in CI */
+  ignoreSnapshots: !process.env['CI'],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
