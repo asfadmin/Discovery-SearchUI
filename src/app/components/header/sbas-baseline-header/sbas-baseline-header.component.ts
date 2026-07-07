1d2e3038f0e6176b0b2cb19078a5230d5ff9fbe7
William Horn <wbhorn@alaska.edu>
2026-07-07 09:11:16 -0800

    feat: size the dataset selector

--- src/app/components/header/sbas-baseline-header/sbas-baseline-header.component.ts
+++ src/app/components/header/sbas-baseline-header/sbas-baseline-header.component.ts
@@ -4,12 +4,19 @@ import { SearchTypeSelectorComponent } from '@components/shared/selectors/search
 import { SearchButtonComponent } from '@components/shared/search-button/search-button.component';
 import { HeaderButtonsComponent } from '../header-buttons/header-buttons.component';
 
+import { TranslateModule } from '@ngx-translate/core';
+import { MatSelectModule } from '@angular/material/select';
+
+import * as models from '@models';
+
 @Component({
   selector: 'app-sbas-baseline-header',
   imports: [
     SearchTypeSelectorComponent,
     SearchButtonComponent,
     HeaderButtonsComponent,
+    MatSelectModule,
+    TranslateModule,
   ],
   templateUrl: './sbas-baseline-header.component.html',
   styleUrls: [
@@ -17,4 +24,23 @@ import { HeaderButtonsComponent } from '../header-buttons/header-buttons.compone
     '../header.component.scss',
   ],
 })
-export class SbasBaselineHeaderComponent {}
+export class SbasBaselineHeaderComponent {
+  datasets = [
+    {
+      name: models.sentinel_1.name,
+      id: models.sentinel_1.id,
+    },
+    {
+      name: models.sentinel_1_bursts.name,
+      id: models.sentinel_1_bursts.id,
+    },
+    {
+      name: 'S1 Multiburst',
+      id: 'S1-MULTIBURST',
+    },
+    {
+      name: models.beta.name,
+      id: models.beta.id,
+    },
+  ];
+}
