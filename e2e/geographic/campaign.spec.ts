7b0cee3a0e4ed0dcc999366c12800e9aeebca16e
Lily Larsen <sglarsen@alaska.edu>
2026-07-23 11:23:08 -0700

    fix: fix campaign filters test

--- e2e/geographic/campaign.spec.ts
+++ e2e/geographic/campaign.spec.ts
@@ -1,10 +1,11 @@
-import { test, expect } from 'e2e/fixtures';
+import { extend_test, expect } from 'e2e/fixtures';
 import { waitForASFAPIResponse } from 'e2e/helpers';
 
-test('Campaign Filter', async ({ page }) => {
+extend_test('Campaign Filter', async ({ page }) => {
   await page.route('**/services/utils/mission_list**', (route) => {
     return route.continue();
   });
+  page.goto('/#/?dataset=SENTINEL-1');
 
   await page.getByRole('button', { name: 'Sentinel-' }).click();
   await page
@@ -25,39 +26,45 @@ test('Campaign Filter', async ({ page }) => {
   );
 });
 
-test('Selecting Multiple Campaigns', { tag: '@visual' }, async ({ page }) => {
-  await page.route('**/services/utils/mission_list**', (route) => {
-    return route.continue();
-  });
-  await page.getByRole('button', { name: 'Sentinel-' }).click();
-  await page
-    .getByRole('menuitem', { name: 'UAVSAR Uninhabited Aerial' })
-    .click();
-  await page.getByRole('button', { name: 'Filters', exact: true }).click();
-  await page
-    .getByRole('textbox', { name: 'Filter Campaign' })
-    .fill('Aleutians');
-  await page
-    .getByRole('listitem')
-    .filter({ hasText: 'Aleutians, AK' })
-    .first()
-    .click();
-  await page
-    .getByRole('textbox', { name: 'Filter Campaign' })
-    .fill('alaska borehole');
-  await page
-    .getByRole('listitem')
-    .filter({ hasText: 'Alaska borehole sites, AK' })
-    .first()
-    .click();
-  await page
-    .locator('app-filters-dropdown')
-    .locator('app-search-button')
-    .getByRole('button', { name: 'SEARCH' })
-    .click();
-  await waitForASFAPIResponse(page);
-  await expect(page.locator('mat-card-title')).toContainText(
-    'UA_permaf_20015_15147_009_151005_L090_CX_02',
-  );
-  await expect(page).toHaveScreenshot();
-});
+extend_test(
+  'Selecting Multiple Campaigns',
+  { tag: '@visual' },
+  async ({ page }) => {
+    await page.route('**/services/utils/mission_list**', (route) => {
+      return route.continue();
+    });
+    page.goto('/#/?dataset=SENTINEL-1');
+
+    await page.getByRole('button', { name: 'Sentinel-' }).click();
+    await page
+      .getByRole('menuitem', { name: 'UAVSAR Uninhabited Aerial' })
+      .click();
+    await page.getByRole('button', { name: 'Filters', exact: true }).click();
+    await page
+      .getByRole('textbox', { name: 'Filter Campaign' })
+      .fill('Aleutians');
+    await page
+      .getByRole('listitem')
+      .filter({ hasText: 'Aleutians, AK' })
+      .first()
+      .click();
+    await page
+      .getByRole('textbox', { name: 'Filter Campaign' })
+      .fill('alaska borehole');
+    await page
+      .getByRole('listitem')
+      .filter({ hasText: 'Alaska borehole sites, AK' })
+      .first()
+      .click();
+    await page
+      .locator('app-filters-dropdown')
+      .locator('app-search-button')
+      .getByRole('button', { name: 'SEARCH' })
+      .click();
+    await waitForASFAPIResponse(page);
+    await expect(page.locator('mat-card-title')).toContainText(
+      'UA_permaf_20015_15147_009_151005_L090_CX_02',
+    );
+    await expect(page).toHaveScreenshot();
+  },
+);
