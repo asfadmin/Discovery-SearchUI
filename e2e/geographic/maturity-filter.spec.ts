292fc049ce0713c4e2c96af6cf94d02841e3268c
Lily Larsen <sglarsen@alaska.edu>
2026-08-10 11:30:54 -0700

    test: improve tests for data maturity filter

--- e2e/geographic/maturity-filter.spec.ts
+++ e2e/geographic/maturity-filter.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from 'e2e/fixtures';
 import { nisarPage } from 'e2e/helpers';
 
-test('Data Maturity', async ({ page }) => {
+test('Data Maturity: Provisional', async ({ page }) => {
   await nisarPage(page);
   await page.getByRole('button', { name: 'Filters', exact: true }).click();
   await page
@@ -13,6 +13,25 @@ test('Data Maturity', async ({ page }) => {
   await page
     .getByRole('textbox', { name: 'Scene Name Patterns' })
     .fill('NISAR_*X*');
+  await expect(
+    page
+      .locator('app-filters-dropdown')
+      .getByRole('button', { name: 'Select max results' }),
+  ).toContainText('of 0 Files');
+});
+
+test('Data Maturity: Beta', async ({ page }) => {
+  await nisarPage(page);
+  await page.getByRole('button', { name: 'Filters', exact: true }).click();
+  await page
+    .locator('div')
+    .filter({ hasText: /^Data Maturity$/ })
+    .first()
+    .click();
+  await page.getByText('Beta').click();
+  await page
+    .getByRole('textbox', { name: 'Scene Name Patterns' })
+    .fill('NISAR_*P*');
   await expect(
     page
       .locator('app-filters-dropdown')
