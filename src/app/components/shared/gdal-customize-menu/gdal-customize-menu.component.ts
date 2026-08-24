cf4e5e52dc579f19a8a64b016d17fc9b4c887f15
Lily Larsen <sglarsen@alaska.edu>
2026-08-24 15:15:27 -0700

    feat: translation strings for copy dataset buttons

--- src/app/components/shared/gdal-customize-menu/gdal-customize-menu.component.ts
+++ src/app/components/shared/gdal-customize-menu/gdal-customize-menu.component.ts
@@ -33,7 +33,7 @@ export class GdalCustomizeMenuComponent {
   openDialog(): void {
     this.dialog.open(GdalCustomizeDialogComponent, {
       data: { product: this.product(), datasets: this.datasets() },
-      minWidth: '80em',
+      maxWidth: '80em',
     });
   }
 }
